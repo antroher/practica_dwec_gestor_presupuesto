c63051ca3a640f8eb7953ad0f97909e3781d343a
Alberto Sánchez Valero <alberto.sanchez@iesdoctorbalmis.com>
2021-10-04 23:35:21 +0700

    Commit Prueba ,Añado primeros pasos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,46 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+'use strict'
+var presupuesto=0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(parametro) {
     // TODO
+    if(parametro>=0)
+    {
+            presupuesto+=parametro;
+            return presupuesto;
+    }else
+    {
+        alert("Error parametro menor de 0");
+        return -1;
+    }
+    
+   
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return "Tu presupuesto actual es de" +presupuesto+ "€";
 }
 
 function CrearGasto() {
-    // TODO
+    // TODO  Deberá comprobar que el valor introducido sea un núḿero no negativo; en caso contrario, asignará a la propiedad valor el valor 0.
+    let gasto = {     // un objeto
+        descripcion: "John",  // En la clave "name" se almacena el valor "John"
+        valor: 30        // En la clave "age" se almacena el valor 30
+      }
+    if(gasto>=0)
+    {
+        return gasto;
+    }else gasto.valor=0;
+
+    
+
 }
+
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
