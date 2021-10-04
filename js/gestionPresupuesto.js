1e8afcf9d121efa35b95b02a8e79e0b13d94d440
Carlos Ramos Mirete <ramos13935@iesmarenostrum.com>
2021-10-04 19:01:48 +0200

    Ramos13935 4/10/2021 19:02

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,46 @@
+"use strict";
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(cantidad) {
     // TODO
+    if (cantidad > -1){
+        presupuesto = cantidad;
+    }else{
+        alert("Error: el presupuesto no puede ser negativo.")
+        presupuesto = -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    alert("Tu presupuesto actual es de " + presupuesto + "€.")
 }
 
-function CrearGasto() {
+function CrearGasto(descr, val) {
     // TODO
+    let Gasto = new Object();
+    Gasto = {
+        descipcion = descr,
+        valor = val,
+    }
+
+    //METODOS OBJETO GASTO
+    Gasto.mostrarGasto = function(){
+        alert("Gasto correspondiente a " + Gasto.descipcion + "con valor " + Gasto.valor +"€")
+    }
+    Gasto.actualizarDescripcion = function(descr){
+        Gasto.descipcion = descr;
+    } 
+    Gasto.actualizarValor = function(val){
+        if (valor >= 0){
+        Gasto.valor = val;
+        }
+    } 
+
+   return Gasto;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
