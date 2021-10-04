b542aa390933a35d8712efaf5dcd75b2701111e9
Alejandro Ortiz Espinosa <alextico612@gmail.com>
2021-10-04 18:18:33 +0200

    Primera tarea

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,39 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto=0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if(parseInt(valor)>0){
+        presupuesto=valor;
+        return presupuesto;
+    }else{
+        alert("ERROR -> Valor negativo.")
+        return -1;
+    } 
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return "Tu presupuesto actual es de "+presupuesto+" €."
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(desc , datoValor) {
+    let v=datoValor;
+    if(parseFloat(valor)<0)v=0;
+    let gasto={
+        descripcion:desc+"",
+        valor:parseFloat(v),
+        mostrarGasto:function(){
+            return "Gasto correspondiente a "+this.descripcion+" con valor "+this.valor+" €.";
+        },
+        actualizarDescripcion:function(desc){
+            if(desc !=null && desc !="") this.descripcion=desc;
+        },
+        actualizarValor:function(dato){
+            if(parseFloat(dato)>0)this.valor=dato;
+        }
+    }
+    return gasto;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
