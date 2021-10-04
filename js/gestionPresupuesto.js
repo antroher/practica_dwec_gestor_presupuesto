071dfc267a5194bbc38205724600c80c6a76c621
David Ortiz <ortiz21983@iesmarenostrum.com>
2021-10-04 19:43:13 +0300

    Primeros metodos creados

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,52 @@
+"Use strict"
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if(parseFloat(valor) > 0){
+        presupuesto = valor;
+        return valor;
+    }else{
+        alert("El valor introducido es un n\xfamero negativo.")
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    alert("Tu presupuesto actual es de " +presupuesto + " €." )
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(desc, val) {
+    let gasto
+    if(parseFloat(valor) > 0){
+        gasto = {
+            descripcion : desc,
+            valor : val
+        } 
+    }else{
+        gasto = {
+            descripcion : desc,
+            valor : 0
+        }
+    }
+    
+
+    gasto.mostrarGasto = function() {
+        alert("Gasto correspondiente a " + this.descripcion + " con valor de " + this.valor + " €.")
+    }
+    gasto.actualizarDescripcion = function(nuevaDesc){
+        this.descripcion = nuevaDesc;
+    }
+
+    gasto.actualizarValor = function(nuevoValor){
+        if(parseFloat(nuevoValor) > 0){
+            this.valor = nuevoValor;
+        }
+    }
+    return gasto;
+    
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
