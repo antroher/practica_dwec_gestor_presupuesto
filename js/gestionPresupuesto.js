8666d383c143747c2d36b7fc3a95cda41e927bd8
Daniela Garcia <garcia19883@iesmarenostrum.com>
2021-10-05 19:25:03 +0200

    05/10/2021_19:24

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,56 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0;
 
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(Numero) {
+let devolver = 0;
+    if(presupuesto >= Numero)
+    {
+        presupuesto = Numero;
+        devolver = presupuesto;
+    }
+    else
+    {
+        console.log("Error, Numero negativo invalido");
+        devolver = -1;
+    }
+    return devolver;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    console.log("Tu presupuesto actual es de" + presupuesto + "€");
+    return ( "siendo" + presupuesto + " el Numero de la variable global presupuesto");
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(Numero,Cadena) {
+    let gasto = {
+        descripcion: Cadena,
+        valor: Numero,
+
+        mostrarGasto(){
+            console.log("Gasto correspondiente a "+ descripcion 
+                +"con " + Numero+" €");
+        },
+        actualizarDescripcion(descripcion){
+            descripcion = descripcion;
+            return descripcion;
+        },
+        actualizarValor(valor){
+            let devolver;
+            if(valor >= 0)
+            {
+                devolver = valor;
+            }
+            return devolver;
+        }
+    }
 }
+
+
+/*tres metodos internos de gastos objeto gasto definido con export{}
+esa funcion devulve dicho objeto */
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
