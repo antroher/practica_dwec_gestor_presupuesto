c69ec1970c975f5354ca514bc982880a9e474ccc
Jose Ramon Jimenez <jimenez21122@iesmarenostrum.com>
2021-10-05 19:01:33 +0200

    initial commit

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,45 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+"use strict"
+var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(presupuesto) {
     // TODO
+    let number = 0;
+    if(presupuesto >= 0)
+    {
+        number = presupuesto;
+    }
+    else
+    {
+        console.log("Los presupuestos son negativos");
+        number = -1;
+    }
+    return number;
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return("Tu presupuesto actual es de " + presupuesto + "€");
 }
 
-function CrearGasto() {
+function CrearGasto(valor) {
     // TODO
+    let gasto = new Object();
+
+    if(valor >= 0)
+    {
+        return gasto;
+    }
+    else{
+        valor = 0;
+    }
+    
+    let gasto = {
+        descipcion
+    }
+    
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
