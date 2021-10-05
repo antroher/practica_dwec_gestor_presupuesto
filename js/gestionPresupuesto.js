99e526de26bc0149c7c02aedcae973090443906b
JPeregrina <garcia21062@iesmarenostrum.com>
2021-10-05 17:10:46 +0200

    Creación funciones

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,33 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
 var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if (valor < 0) {
+        alert("El valor introducido es negativo e incorrecto.");
+        return -1;
+    }
+    else {
+        presupuesto = valor;
+        return presupuesto;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    alert(`Tu presupuesto actual es de ${presupuesto} €.`);
 }
 
-function CrearGasto() {
-    // TODO
-}
+function CrearGasto(descripcion, valor) {
+    if (valor < 0) {
+        valor = 0;
+    }
+    
+    return gasto = {
+        descripcion: descripcion,
+        valor: valor
+    };
+} 
+
+let gasto = CrearGasto("Tintoreria", 4500);
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
