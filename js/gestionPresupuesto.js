16e0fe483e1a3684e890543e389d0c51c49f2412
Noelia Pol <noeliapol@hotmail.com>
2021-10-05 11:41:04 +0200

    2021/10/05-11:41

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -47,6 +47,26 @@ function detectarNegat(num)
         num=false;
 }
 
+function mostrarGasto()
+{
+    alert `Gasto correspondiente a ${gasto.descripcion} con valor VALOR €`
+}
+
+function actualizarDescripcion(desc)
+{
+    gasto.descripcion=desc;
+}
+
+function actualizarValor()
+{
+    if(detectarNegat(val)==true)
+    {
+        gasto.valor=v;
+    }
+}
+
+
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
