26ca26a882f64da22ae301d7d79097d00020f992
Raul Molina <molina21182@iesmarenostrum.com>
2021-10-05 19:02:10 +0200

    Primera versión

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,48 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0;
 
 
-function actualizarPresupuesto() {
+function actualizarPresupuesto(presupuesto) {
     // TODO
+    let a = prompt("Introduzca el valor del presupuesto: ");
+
+    if(isNaN(a))
+    {
+        console.log("ERROR El valor introducido no es un número");
+        return -1;
+    }
+    else
+    {
+        if (a > 0)
+        {
+            presupuesto = a;
+            return presupuesto;
+        }
+        else
+        {
+            console.log("ERROR El valor introducido es negativo");
+            return -1;
+        }
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    let mensaje = ("Tu presupuesto actual es de " + presupuesto + " €");
+    return mensaje;
 }
 
 function CrearGasto() {
     // TODO
+    let gasto = new Object();
+    let valor = 0;
+
+    if(gasto < 0)
+        valor = gasto;
+    else
+        return gasto;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
