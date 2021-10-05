1ee7b8153c437c258ee134499a2dfde39096e096
Ismael Feliu <feliu21025@iesmaernostrum.com>
2021-10-05 18:26:49 +0200

    añadidas varias funciones

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,18 +2,29 @@
 
 // TODO: Variable global
 
+var presupuesto = 0;
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(numero) {
+    if(numero >= 0)
+        return numero;
+    else
+        console.log("ERROR. Valor no valido");
+        return -1;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return "Tu presupuesto actual es de "+presupuesto+" €";
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(valor, descripcion) {
+    if(actualizarPresupuesto(valor) != -1)
+    {
+        _valor = 0;
+    } 
+    _valor = valor;
+    _descripcion = descripcion;
 }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
