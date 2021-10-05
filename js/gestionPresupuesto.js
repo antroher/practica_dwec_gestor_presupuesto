fb16d33a934f4017a7676c32f0f2356e276dad72
fabioladurann <72142783+fabioladurann@users.noreply.github.com>
2021-10-05 18:52:05 +0200

    18:51

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,34 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto() 
+{
+    if(presupuesto >= valor) 
+    {
+        return presupuesto;
+    }
+    else
+    {
+        return -1;
+    }
 }
 
-function mostrarPresupuesto() {
-    // TODO
+function mostrarPresupuesto() 
+{
+    return("Tu presupuesto actual es de " + presupuesto + "€");
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(valor, descripcion) 
+{
+    if(actualizarPresupuesto(valor)!= 1)
+    {
+        _valor = 0;
+    }
+
+    _valor = valor;
+    _descripcion = descripcion;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
