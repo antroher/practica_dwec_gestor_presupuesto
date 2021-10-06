a151eb2824ec735efb0e5f530b55f467348add5d
JesusBonete <bonete20963@iesmarenostrum.com>
2021-10-06 18:25:35 +0200

    Hasta Crear Gasto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,34 @@
+
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto=0;
 
+function actualizarPresupuesto(cantidad) {
+    // TODO 
+    if(valor<0){
+        cantidad=-1; 
+        console.log("Error al introducir el valor")       
+    }
+    else{
+        presupuesto=cantidad;
+    }
+    return presupuesto;
 
-function actualizarPresupuesto() {
-    // TODO
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    let x = presupesto;
+    console.log("Tu presupuesto actual es: " + x + "€")
 }
 
 function CrearGasto() {
     // TODO
+    let gasto = {
+        descripcion: " ",
+        valor: 0,
+    }
+
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
