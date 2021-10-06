bc6faf0ed4fcbc931edfad1f0840df782d7ff425
Senen Rodenas <71589700+Senen1997@users.noreply.github.com>
2021-10-06 07:57:21 +0200

    hola5

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,6 +1,6 @@
-// "use strict"
+"use strict"
 
-// // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 // class Gasto {
 //     constructor (descripcion, valor) {
 //         this.descripcion = descripcion;
@@ -21,108 +21,50 @@
 //         }
 //     }
 // }
-// // TODO: Variable global
-// var presupuesto = 0;
-
-// function actualizarPresupuesto(presupuesto1) {
-//     if (presupuesto1 > 0) {
-//         presupuesto = presupuesto1;
-//         return presupuesto;
-//     } else {
-//         console.log("Error, el valor es negativo");
-//         return -1;
-//     }
-// }
-
-// function mostrarPresupuesto() {
-//     return(`Tu presupuesto actual es de ${presupuesto} €`);
-// }
-
-// function CrearGasto(descripcion, valor) {
-//     if (parseFloat(valor) < 0) {
-//         valor = 0;
-//     }
-
-//     this.valor = valor;
-//     this.descripcion = descripcion;
-
-//     this.mostrarGasto = function() {
-//         console.log(`Gasto correspondiente a ${descripcion} con valor ${valor} €`);
-//     }
-
-//     this.actualizarDescripcion = function(descripcion) {
-//         this.descripcion = descripcion;
-//     }
-
-//     this.actualizarValor = function(newValor) {
-//         if (newValor > 0) {
-//             this.valor = newValor;
-//         }
-//     }
-//     // return CrearGasto;
-// }
-
-// // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
-// // Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-// export   {
-//     mostrarPresupuesto,
-//     actualizarPresupuesto,
-//     CrearGasto
-// }
-
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
 // TODO: Variable global
 var presupuesto = 0;
 
-function actualizarPresupuesto(valor) {
-    // TODO
-    let valorDevolver;
+function actualizarPresupuesto(newPresupuesto) {
+    let newValor;
 
-    if(valor >= 0){
-        presupuesto = parseFloat(valor);
-        valorDevolver = presupuesto;
+    if (newPresupuesto >= 0) {
+        presupuesto = parseFloat(newPresupuesto);
+        newValor = presupuesto;
+    } else {
+        console.log("Error. Valor introducido no valido.");
+        newValor = -1;
     }
-    else{
-        console.log("Error. Valor introducido no valido.")
-        valorDevolver = -1;
-    }
-return valorDevolver;
+    return newValor;
 }
 
 function mostrarPresupuesto() {
-    // TODO
-    return `Tu presupuesto actual es de ${presupuesto} €`;
+    return(`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
 function CrearGasto(descripcion, valor) {
-    // TODO
-    if(valor < 0 || isNaN(valor)){
+    if (isNaN(valor) || valor < 0) {
         valor = 0;
     }
 
     let gasto = {
-        descripcion: descripcion,
-        valor: valor,
+        valor : valor,
+        descripcion : descripcion,
 
-        mostrarGasto(){
-            return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+        mostrarGasto : function() {
+            return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
         },
 
-        actualizarDescripcion(nuevaDescripcion){
-            this.descripcion = nuevaDescripcion;
+        actualizarDescripcion : function(newDescripcion) {
+            this.descripcion = newDescripcion;
         },
 
-        actualizarValor(nuevoValor){
-            let valorDevuelto
-
-            if(nuevoValor >= 0){
-                this.valor = nuevoValor;
+        actualizarValor : function(newValor) {
+            if (newValor >= 0) {
+                this.valor = newValor;
             }
         }
     };
-
+    
     return gasto;
 }
 
@@ -134,3 +76,4 @@ export   {
     actualizarPresupuesto,
     CrearGasto
 }
+
