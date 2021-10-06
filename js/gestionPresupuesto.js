522b648fade3a0344c82204b14997c51b4d998f4
Angel y alfonso <alfonso20545@iesmarenostrum.com>
2021-10-06 19:01:56 +0200

    commit sin errores

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -5,49 +5,53 @@
 var presupuesto = 0;
 function actualizarPresupuesto(valores) {
 
-    if(parseFloat(valores) > -1)
-    {
+    if (parseFloat(valores) >= 0) {
         presupuesto = valores;
         return valores;
-    }else{
+    } else {
         console.log("ha introducido un numero negativo");
         return -1;
     }
 }
 
 function mostrarPresupuesto() {
-    return 'Tu presupuesto actual es de '+ presupuesto +' €';
+    return 'Tu presupuesto actual es de ' + presupuesto + ' €';
 }
 
-function CrearGasto(Val, Descripcion) {
-    let gasto;
-    if(parseFloat(Val) > -1){
-     gasto = {
-        valor: Val,
-        descripcion: Descripcion
-    };
-}else{
+function CrearGasto(descr, val) {
 
-     gasto = {
-        descripcion: Descripcion,
-        valor: 0
-    }; }
-    gasto.mostrarGasto = function (){
-        return 'Gasto correspondiente a ' + descripcion+ ' con valor VALOR '+ valor +'€';
-        }
-    gasto.actualizarDescripcion = function(actualizarDescripcion){
-        gasto.descripcion = actualizarDescripcion;
-        }
-    gasto.actualizarValor = function (actualizarValor){
-        gasto.valor = actualizarValor;
-        }
-    if(parseFloat(valor) > 0)
-    {
-        gasto = valor;
-        return valor;
-    }else{
-        valor = 0;
+    let gasto;
+
+    if (parseFloat(val) >= 0) {
+        gasto = {
+            descripcion: descr,
+            valor: val           
+        };
     }
+
+    else {
+
+        gasto = {
+            descripcion: descr,
+            valor: 0
+        };
+    }
+
+    gasto.mostrarGasto = function () {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    };
+
+    gasto.actualizarDescripcion = function (newDescr) {
+        this.descripcion = newDescr;
+    };
+
+    gasto.actualizarValor = function (v1) {
+        if (parseFloat(v1) >= 0) {
+            this.valor = v1;
+        } 
+    };
+
+    
     return gasto;
 }
 
@@ -57,7 +61,7 @@ function CrearGasto(Val, Descripcion) {
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
