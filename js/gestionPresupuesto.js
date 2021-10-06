691ac3d1bcc661e90e145cd20841947ba9c12ca1
Pablo Penalver <penalverpablo@gmail.com>
2021-10-06 17:31:03 +0200

    ejercicio presupuesto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -3,26 +3,53 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
-var presupuesto = 0;
+let presupuesto = 0;
 
-function actualizarPresupuesto(nuevoValor) {
-    // TODO
-
-    return;
+function actualizarPresupuesto(cantidad) {
+    if (cantidad >= 0) {
+        presupuesto = cantidad;
+    } else {
+        cantidad = -1;
+        console.log("Error al introducir el valor")
+    }
+    return cantidad;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+
+    let x = presupuesto;
+    return `Tu presupuesto actual es de ${x} €`
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    
+    this.descripcion = descripcion;
+    if(valor>=0) {
+        this.valor = valor;
+    }else{
+        this.valor = 0;
+    }
+    
+
+    this.mostrarGasto = function () {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    }
+    this.actualizarDescripcion = function(nuevaDescripcion) {
+        this.descripcion=nuevaDescripcion;
+    }
+    this.actualizarValor = function(nuevoValor){
+        if(nuevoValor>=0){
+            this.valor=nuevoValor;
+        }
+
+    }
+  
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
