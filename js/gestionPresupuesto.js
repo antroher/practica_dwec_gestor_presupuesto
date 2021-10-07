9208ad5640e6ed1a40c1431884037a4765f4fd81
Ricardo Hernandez <hernandez16437@iesmarenostrum.com>
2021-10-07 20:21:09 +0200

    07/10/2021 20:20

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -5,14 +5,11 @@ var presupuesto = 0;
 
 function actualizarPresupuesto(num) {
     // TODO
-    let retorno = -1;
+    let retorno ;
 
-    if(isNaN(num))
-    {
+    if(isNaN(num)){
         retorno = -1;
-    }
-    else
-    {    
+    } else {    
         if(num < 0)
         {
             console.log('El dato introducido tiene que ser positivo');
@@ -36,21 +33,32 @@ function mostrarPresupuesto() {
 
 function CrearGasto(desintro, valorintro) {
     // TODO
-    if(valorintro < 0)
+    if(valorintro < 0 || isNaN(valorintro))
     {
         valorintro = 0;
     }
 
     let gasto =
     {
-        description: desintro,
-        valor: valorintro
+        descripcion : desintro,
+        valor : valorintro,
+        mostrarGasto : function(){
+            return (`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
+        },
+        actualizarDescripcion : function(description2){ 
+            this.descripcion = description2;
+        },
+        actualizarValor : function(valor2){ 
+            if(valor2 > 0)
+            {
+                this.valor = valor2;
+            }
+        }
+    
     };
 
     return gasto;
 }
-
-   
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
