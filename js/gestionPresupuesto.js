14d6e8cd4de0ea9981153cb4adb2095fe9412648
Robert Hernández <hernandez19875@iesmarenostrum.com>
2021-10-07 20:28:22 +0200

    07102021_20:32

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,10 +1,16 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+"use strict";
 
-// TODO: Variable global
+
+var presupuesto = 0; // TODO: Variable global
 
 
 function actualizarPresupuesto() {
-    // TODO
+    let num = parseInt(prompt("Introduce un valor positivo"));
+    if (num < 0) {
+        num = -1;
+        alert(`ERROR. El valor debe ser positivo`);
+    }
 }
 
 function mostrarPresupuesto() {
@@ -18,7 +24,7 @@ function CrearGasto() {
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export   {
+export {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto
