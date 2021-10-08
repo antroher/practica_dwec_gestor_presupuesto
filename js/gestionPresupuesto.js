979f28419d3b08fd853fc88812d744e85b45d332
Victor Bellver <bellver20952@iesmarenostrum.com>
2021-10-08 20:05:08 +0200

    4th Commit --  Todas las funciones terminadas, faltan los test

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -30,10 +30,38 @@ function mostrarPresupuesto() {
     return (`Presupuesto actual: ${budget}€`);
 }
 
-function CrearGasto() {
+function CrearGasto(value, description) {
     // TODO
+    if(value1 < 0 || isNaN(value1)){
+
+        value1 = 0;
+    }//Con esto comprobamos que value1 no sea negativo ni sea un string
     
-}
+    let expenditure = { //ESTAR ATENTO CON LOS = CUANDO SE DECLARAN CONSTRUCTORES
+        value: value, 
+        description: description, //Esto hace referencia a las propiedades que tiene el objeto y se le asignan por parametro una vez recurrimos al constructor
+        
+        //A continuación los métodos que van ligados al constructor
+        mostrarGasto(){
+            console.log (`Gasto correspondiente a ${this.description} con valor ${this.value} €.`);
+            return (`Gasto correspondiente a ${this.description} con valor ${this.value} €.`);
+        },
+        actualizarDescripcion(nDescription){
+            this.description = nDescription;
+        },
+        actualizarValor(nValue){
+            if(value1 < 0 || isNaN(value1)){
+
+                console.log("El valor introducido no es correcto, introduzca un numero positivo");
+
+            }else{
+                this.value = nValue;
+            }
+        }
+        
+    };
+    return expenditure
+} 
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
