2127cc41ba49e7fea8316267d7ba552d1575b23e
Yeray Miguel <miguel19699@iesmarenostrum.com>
2021-10-08 19:34:17 +0200

    0910211934

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,46 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+"use strict"
+var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(newPresupuesto) {
+    if(newPresupuesto >= 0){
+        presupuesto = newPresupuesto;
+    } else {
+        console.log("Error presupuesto negativo ");
+        return -1;
+    }
+    return presupuesto;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return (`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    if(valor < 0 || isNaN(valor)){
+        valor = 0;
+    }
+    let gasto = {
+        valor : valor,
+        descripcion : descripcion,
+        mostrarGasto : function() {
+            return(`Gasto correspondiente a ${this.descripcion} con valor ${valor} €`);
+        },
+
+        actualizarDescripcion : function(descripcion) {
+            this.descripcion = descripcion;
+        },
+
+        actualizarValor : function(valor) {
+            if(valor > 0) {
+                this.valor = valor;
+            }
+        }
+    }
+
+    return gasto;
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
