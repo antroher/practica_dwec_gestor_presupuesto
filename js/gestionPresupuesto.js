7f3b65bc04eaacfd860ae0578eab33226bdd7369
Marina Paya <marinapaya1917@gmail.com>
2021-10-08 15:14:08 +0200

    sdgfsg

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,55 @@
+import { strict } from "assert";
+
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+'use strict'
 
+var presupuesto = 0;
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if (valor >= 0) {
+        presupuesto = valor;
+    } else {
+       console.log('Error. Presupuesto negativo');
+       valor = -1;
+    }
+    return valor;
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return(`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(desc, val) {
+    if (val < 0 || isNaN(val)) {
+        val = 0;
+    }
+
+    let gasto = {
+	descripcion: desc,
+        valor : val,
+        mostrarGasto : function (){
+            return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
+        },
+
+        actualizarDescripcion : function(descr) {
+            this.descripcion = descr;
+        },
+
+        actualizarValor : function(vall) {
+            if (vall >= 0)
+            {
+                this.valor = vall;
+            }
+        }
+    };
+    return gasto;
 }
+
+    
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
