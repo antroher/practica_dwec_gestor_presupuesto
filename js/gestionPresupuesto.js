1d3248608b97cff5cd19d0e72d729a99d35f5c09
Victor Bellver <bellver20952@iesmarenostrum.com>
2021-10-08 19:30:04 +0200

    2nd Commit -- Primera funcion terminada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,10 +1,27 @@
+"use strict";
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
-
+var budget = 0;
 //COMENTARIO PARA PRIMER COMMIT Y COMPROBAR VERSIONES
-function actualizarPresupuesto() {
-    // TODO
+
+function actualizarPresupuesto(value) {
+    //TODO
+    let comeBack = 0;
+    if(value < 0){
+        console.log("ERROR, es un número negativo");
+        comeBack = -1;
+    }
+    else if(isNaN(value))
+    {
+        console.log("ERROR, carácter invalido");
+        comeBack = -1;
+    }else{
+        budget = value;
+        comeBack = buget;
+    }
+    return comeBack;
+    
 }
 
 function mostrarPresupuesto() {
