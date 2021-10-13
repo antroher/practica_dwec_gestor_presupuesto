4451e01a2e8f7cb8fe21a3877771b4788c246be2
JPeregrina <garcia21062@iesmarenostrum.com>
2021-10-13 15:06:51 +0200

    Adición de Parametros a crearGasto y funciones a Exportar

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var presupuesto = 0;
+var gastos = [];
+var idGasto = 0;
 
 function actualizarPresupuesto(valor) {
     let valorDevuelto = 0;
@@ -22,14 +24,34 @@ function mostrarPresupuesto() {
     return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto(descripcionEntrante, valorEntrante) {
+function CrearGasto(descripcionEntrante, valorEntrante, fechaEntrante, etiquetasEntrante) {
     if (valorEntrante < 0 || isNaN(valorEntrante)) {
         valorEntrante = 0;
+    }
+    if (etiquetasEntrante === "") {
+        etiquetasEntrante = []
+    }
+    if (fechaEntrante === "") {
+        let fechaObjeto = new Date();
+        let fechaActual = fechaObjeto.getDate() + "/" + (fechaObjeto.getMonth() + 1) + "/" + fechaObjeto.getFullYear() + "T" + 
+            fechaObjeto.getHours() + ":" + fechaObjeto.getMinutes();
+        fechaEntrante = Date.parse(fechaActual);
+    }
+    else if (isNaN(Date.parse(fechaEntrante))){
+        let fechaObjeto = new Date();
+        let fechaActual = fechaObjeto.getDate() + "/" + (fechaObjeto.getMonth() + 1) + "/" + fechaObjeto.getFullYear() + "T" + 
+            fechaObjeto.getHours() + ":" + fechaObjeto.getMinutes();
+        fechaEntrante = Date.parse(fechaActual);
+    }
+    else {
+        fechaEntrante = Date.parse(fechaEntrante);
     }
     
     let gasto = {
         descripcion: descripcionEntrante,
         valor: parseFloat(valorEntrante),
+        etiquetas: etiquetasEntrante,
+        fecha: fechaEntrante,
 
         mostrarGasto() {
             console.log(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
@@ -50,11 +72,30 @@ function CrearGasto(descripcionEntrante, valorEntrante) {
     return gasto;
 } 
 
+function listarGastos() {
+    return gastos;
+}
+
+function anyadirGasto() {
+
+}
+
+function borrarGasto() {}
+
+function calcularTotalGastos() {}
+
+function calcularBalance() {}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
