2cefe24765971a7dd83a798a99d845a1aca9b0f5
Alina Maltceva <maltceva20537@iesmarenostrum.com>
2021-10-13 19:04:11 +0200

    attempt 1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 // TODO: Variable global
 
 let presupuesto = 0;
+let gastos = new Array();
+let idGasto = 0;
 
 function actualizarPresupuesto(valor)
 {
@@ -41,11 +43,41 @@ function CrearGasto(discount, value)
     return expense;
 }
 
+function listarGastos()
+{
+
+}
+
+function anyadirGasto()
+{
+
+}
+
+function borrarGasto()
+{
+
+}
+
+function calcularTotalGastos()
+{
+
+}
+
+function calcularBalance()
+{
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
