ac0ab60599de9cfdd18f5d66b6af272ee77143fb
Ismael Amoros <amoros20650@iesmarenostrum.com>
2021-10-15 19:30:07 +0200

    Creando funciones
    He creado las nuevas variables globales y las nuevas funciones, falta completarlas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,5 +1,7 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 var presupuesto = 0;
+var gastos = new Array();
+var idGasto = 0;
 // TODO: Variable global
 
 
@@ -51,11 +53,36 @@ function CrearGasto(descripcionIn,valorIn) {
     return gasto;
 }
 
+function listarGastos(){
+
+}
+
+function anyadirGasto(){
+
+}
+
+function borrarGasto(){
+
+}
+
+function calcularTotalGastos(){
+
+}
+
+function calcularBalance(){
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
