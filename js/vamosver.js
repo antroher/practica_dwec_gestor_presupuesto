1d2d953736e9f7384932019295caff1e59c683bb
JPeregrina <garcia21062@iesmarenostrum.com>
2021-10-15 17:03:53 +0200

    Borrado de js

--- js/vamosver.js
+++
@@ -1,9 +0,0 @@
-function crearFecha() {
-    let fechaObjeto = new Date();
-        let fechaActual = fechaObjeto.getDate() + "/" + (fechaObjeto.getMonth() + 1) + "/" + fechaObjeto.getFullYear() + "T" + 
-            fechaObjeto.getHours() + ":" + fechaObjeto.getMinutes();
-    return   fechaActual
-}   
-
-let fecha = crearFecha();
-console.log(fecha);
