4134541ebbdadd2eafa1bc7bbf40478c112e983e
Ismael Feliu <feliu21025@iesmaernostrum.com>
2021-10-15 18:44:15 +0200

    algunos cambios

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -21,7 +21,7 @@ function mostrarPresupuesto() {
     return (`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, fecha = Date.now(), ...etiquetas) {
     if(valor <= 0 || isNaN(valor))
     {
         valor = 0;
@@ -30,6 +30,8 @@ function CrearGasto(descripcion, valor) {
     let gasto = {
         valor: valor,
         descripcion: descripcion,
+        etiquetas: [...etiquetas],
+        fecha: (typeof fecha === 'string') ? Date.parse(fecha) : fecha,
         mostrarGasto() {
             return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
         },
@@ -41,17 +43,54 @@ function CrearGasto(descripcion, valor) {
         actualizarValor(NValor) {
             if(NValor > 0)
                 this.valor = NValor;
+        },
+
+        anyadirEtiquetas(...Netiquetas){
+            for(var i = 0; i < this.etiquetas.length; i++)
+            {
+                if(Netiquetas != this.etiquetas[i])
+                    this.etiquetas = [...Netiquetas];
+            }
+        },
+
+        mostrarGastoCompleto(){
+            return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} € 
+            Fecha: ${this.fecha}
+            Etiquetas: 
+            - ${this.etiquetas[1]} 
+            - ${this.etiquetas[2]} 
+            - ${this.etiquetas[3]}`;
+        },
+
+        actualizarFecha(Nfecha){
+            fecha = (typeof Nfecha == 'string') ? Date.parse(Nfecha) : Nfecha;
         }
     }
 
     return gasto;
 }
 
-
     function listarGastos(){
         return gastos;
     }
 
+    function anyadirGasto(gasto){
+        gasto = {
+            id: idGasto
+        }  
+    }
+
+    function borrarGasto(id){
+
+    }
+
+    function calcularTotalGastos(){
+
+    }
+
+    function calcularBalance(){
+
+    }
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
