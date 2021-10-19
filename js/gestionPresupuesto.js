5a7b2152cad51c611e2a50ab8dc74b896aeee2a1
Jesús Sansano <sansano21276@iesmarenostrum.com>
2021-10-19 19:16:03 +0200

    commit final espero.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 
 // TODO: Variable global
 var presupuesto = 0;
+var gastos = [];
+var IDGasto = 0;
 
     function actualizarPresupuesto(valorIntroducido) {
     // TODO
@@ -23,7 +25,7 @@ var presupuesto = 0;
         return`Tu presupuesto actual es de ${presupuesto} €`;
     }
 
-    function CrearGasto(desc,numIntroducido) {
+    function CrearGasto(desc,numIntroducido,fecha = Date.now(),...etiq) {
         
         // TODO
         if(numIntroducido <= 0 || isNaN(numIntroducido)){
@@ -32,8 +34,9 @@ var presupuesto = 0;
             let gasto = {
                 descripcion:desc,
                 valor:numIntroducido,
+                fecha : (typeof fecha === "string") ? Date.parse(fecha) : fecha,
+                etiquetas : [...etiq],
             mostrarGasto(){
-                console.log(`Gasto correspondiente a Ejemplo de gast ${gasto.descripcion} y con Valor ${gasto.valor}`);
                 return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`
             },
             actualizarDescripcion(desc){
@@ -42,12 +45,88 @@ var presupuesto = 0;
             actualizarValor(numIntroducido){
                 if(numIntroducido >= 0)
                     this.valor = parseFloat(numIntroducido); 
-            }
+            },
+            actualizarFecha(stringFecha){
+                if(!isNaN(Date.parse(stringFecha)))              
+                    this.fecha = Date.parse(stringFecha);           
+            },
+            anyadirEtiquetas (...etiquetas) {
+                let valoresUnicos = etiquetas.filter((x) => { // el .filter lo vamos a usar para generar un nuevo array con todos los elementos que cumplan con la condición que le hemos puesto.
+                    if (!this.etiquetas.includes(x))  // el . includes nos dice si el elemento (x) se encuentra dentro del array devolviendo true o false según corresponda.
+                        return x;                   
+                });
+                this.etiquetas.push(...valoresUnicos);
+            }, 
+    
+            borrarEtiquetas (...etiquetas) {
+                etiquetas.forEach((x) => { // x es por cada etiqueta dentro del array etiquetas que le aplique la lógica. utilizada.
+                    for (let i = 0; i < this.etiquetas.length; i++) {
+                        if (this.etiquetas[i] === x) 
+                            this.etiquetas.splice(i, 1); // Cambia el contenido del array eliminando elementos existentes y añadimos otros nuevos.
+                        
+                    }
+                })
+            },
+            mostrarGastoCompleto : function() {
+                let fec1;
+                    if(typeof this.fecha === 'string')                
+                        fec1 = Date.parse(this.fecha);                  
+                    else
+                        fec1 = this.fecha;                    
+                let aux = "";
+                    for(let etiqueta of this.etiquetas) { // Sacamos todas las etiquetas con --> for (let ... of...) Iteramos un array con un objecto creado para ir variando su valor en este caso etiqueta va a coger el valor de cada etiqueta en cada repetición.
+                        aux = aux + `- ${etiqueta}\n`;
+                    };
+        
+                let fec2 = new Date(fec1);
+    
+                let texto = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.\nFecha: ${(fec2.toLocaleString())}\nEtiquetas:\n`;
+                return texto + aux;
+            },
         }
 
         return gasto;
 
     }
+
+    function listarGastos() {
+
+            return gastos;
+    }
+
+    function anyadirGasto(gasto) {
+        gasto.id = IDGasto;
+        IDGasto++;
+        gastos.push(gasto);
+    }
+
+    function borrarGasto(IDGasto) {
+            gastos.forEach(x => {
+                if(x.id == IDGasto )           
+                gastos.splice(gastos.indexOf(x),1); // El splice es lo que explicó antonio en clase pero tengo que volver a preguntarle el funcionamiento.     
+            })
+          
+        
+    }
+
+    function calcularTotalGastos(){
+        let sumaGastos = 0;
+
+        gastos.forEach((x) => sumaGastos += x.valor); // Iteramos sobre la lista de gastos con el método forEach en el que x va ir valiendo en cada repetición el valor de cada gasto y luego simplemente acumulamos el número del parámetro --> gasto.valor <-- en una variable auxiliar.
+
+        return sumaGastos;    
+        
+    }
+
+    function calcularBalance() {
+        let totalGastos = calcularTotalGastos();
+
+        let balance = presupuesto - totalGastos;
+
+        return balance;
+    }
+
+
 
 
 
@@ -57,5 +136,11 @@ var presupuesto = 0;
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance,
+
 }
