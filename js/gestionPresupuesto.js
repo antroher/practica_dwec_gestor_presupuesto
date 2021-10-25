eecb0d5080e72f9170d555434a75c5b0fa8482b0
Carlos Ramos Mirete <ramos13935@iesmarenostrum.com>
2021-10-25 16:30:22 +0200

    CRM Commit Tests correctos Fundamentos JS III 25/10/2021

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -26,11 +26,10 @@ function mostrarPresupuesto() {
 
 function CrearGasto(descr, val, fech = Date.now(), ...etiq) {
   //Comprobar que la fecah se puede pasar a formato fecha
-  if (!Date.parse(fech)){
+  if (!Date.parse(fech)) {
     fech = Date.now();
-  }
-  else{
-    fech = Date.parse(fech)
+  } else {
+    fech = Date.parse(fech);
   }
 
   //Comprobar que valor es un numero y comproprobar que no es menor que 0 ni una cadena.
@@ -42,7 +41,7 @@ function CrearGasto(descr, val, fech = Date.now(), ...etiq) {
     descripcion: descr,
     valor: val,
     fecha: fech,
-    etiquetas: [...etiq]
+    etiquetas: [...etiq],
   };
 
   //METODOS OBJETO GASTO
@@ -67,7 +66,7 @@ function CrearGasto(descr, val, fech = Date.now(), ...etiq) {
   };
 
   gasto.mostrarGastoCompleto = function () {
-   let nfec = new Date(this.fecha).toLocaleString();
+    let nfec = new Date(this.fecha).toLocaleString();
     let textoADevolver =
       "Gasto correspondiente a " +
       gasto.descripcion +
@@ -75,7 +74,8 @@ function CrearGasto(descr, val, fech = Date.now(), ...etiq) {
       gasto.valor +
       " €.\n" +
       "Fecha: " +
-     nfec + "\n" +
+      nfec +
+      "\n" +
       "Etiquetas:\n";
     for (let i = 0; i < this.etiquetas.length; i++) {
       textoADevolver += "- " + this.etiquetas[i] + "\n";
@@ -95,14 +95,34 @@ function CrearGasto(descr, val, fech = Date.now(), ...etiq) {
         gasto.etiquetas.push(nuevaEtiqueta[i]);
     }
   };
+
   gasto.borrarEtiquetas = function (...listaEtiquetasABorrar) {
     for (let i of listaEtiquetasABorrar) {
       if (gasto.etiquetas.includes(i)) {
         let indice = gasto.etiquetas.indexOf(i);
-        gasto.etiquetas.splice(indice, 1)
+        gasto.etiquetas.splice(indice, 1);
       }
     }
   };
+
+  gasto.obtenerPeriodoAgrupacion = function (periodo) {
+    let resultado = "0";
+    let fechObj = new Date(this.fecha);
+    switch (periodo) {
+      case "dia":
+        return fechObj.toISOString().substring(0, 10);
+      case "mes":
+        return fechObj.toISOString().substring(0, 7);
+      case "anyo":
+        return fechObj.toISOString().substring(0, 4);
+      default:
+        console.log("Periodo erroneo.");
+        break;
+    }
+    
+    return resultado;
+  };
+
   return gasto;
 }
 /** FUNCIONES FUNCIONES Fundamentos de JavaScript II **/
@@ -117,18 +137,17 @@ function anyadirGasto(objetoGasto) {
 }
 
 function borrarGasto(id) {
-
   for (let key of gastos) {
     if (key.id === id) {
       let indice = gastos.indexOf(key);
-      gastos.splice(indice,1);
+      gastos.splice(indice, 1);
       break;
     }
   }
 }
 function calcularTotalGastos() {
   let totalGastos = 0;
-  for (let i in gastos){
+  for (let i in gastos) {
     totalGastos += gastos[i].valor;
   }
   return totalGastos;
@@ -137,6 +156,137 @@ function calcularBalance() {
   return presupuesto - calcularTotalGastos();
 }
 
+function filtrarGastos(objetoFiltro) {
+  if (
+    objetoFiltro != undefined &&
+    objetoFiltro != null &&
+    Object.entries(objetoFiltro).length != 0
+  ) {
+    let resultado = gastos.filter((gast) => {
+      if (objetoFiltro.hasOwnProperty("fechaDesde")) {
+        if (gast.fecha < Date.parse(objetoFiltro.fechaDesde)) {
+          return;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("fechaHasta")) {
+        if (gast.fecha > Date.parse(objetoFiltro.fechaHasta) ) {
+          return;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("valorMinimo")) {
+        if (gast.valor < objetoFiltro.valorMinimo) {
+          return;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("valorMaximo")) {
+        if (gast.valor > objetoFiltro.valorMaximo) {
+          return;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("descripcionContiene")) {
+        if (!gast.descripcion.includes(objetoFiltro.descripcionContiene)) {
+          return;
+        }
+      }
+      if (objetoFiltro.hasOwnProperty("etiquetasTiene")) {
+        if ( objetoFiltro.etiquetasTiene.length != 0){
+        let check = false;
+        for (let des of objetoFiltro.etiquetasTiene) {
+          if (gast.etiquetas.includes(des)) {
+            check = true;
+          }
+        }
+        if (!check) {
+          return;
+        }
+      }
+    }
+      return gast;
+    });
+
+    return resultado;
+
+  } else {
+    return gastos;
+  }
+
+  /******FUNCIÓN  filtrarGastos RESUELTA SIN .filter******/
+  /*  let resultado = [];
+  if ((objetoFiltro != undefined && objetoFiltro != null && Object.entries(objetoFiltro).length != 0)) {
+    for (let gast of gastos) {
+      if (objetoFiltro.hasOwnProperty("fechaDesde")) {
+        if (gast.fecha < Date.parse(objetoFiltro.fechaDesde)) {
+          continue;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("fechaHasta")) {
+        if (gast.fecha > Date.parse(objetoFiltro.fechaHasta)) {
+          continue;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("valorMinimo")) {
+        if (gast.valor < objetoFiltro.valorMinimo) {
+          continue;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("valorMaximo")) {
+        if (gast.valor > objetoFiltro.valorMaximo) {
+          continue;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("descripcionContiene")) {
+        if (!gast.descripcion.includes(objetoFiltro.descripcionContiene)) {
+          continue;
+        }
+      }
+
+      if (objetoFiltro.hasOwnProperty("etiquetasTiene")) {
+        let flag = false;
+        for (let des of objetoFiltro.etiquetasTiene) {
+          if (gast.etiquetas.includes(des)) {
+            flag = true;
+          }
+        }
+        if (!flag) {
+          continue;
+        }
+      }  
+    resultado.push(gast);
+
+    }
+    return resultado
+
+  } else {
+    return gastos;
+  }*/
+}
+
+function agruparGastos(periodo = "mes", etiquetas = [], fechDesd, fechaHas = Date.now()) {
+  
+  //if (fechDesd == undefined){
+    //let aux = new Date(Date.now()).getFullYear();
+    //fechDesd = 0;
+  //}
+    let listaResultadoFiltros = filtrarGastos({fechaDesde: fechDesd, fechaHasta:fechaHas, etiquetasTiene: etiquetas});
+    let gastosAgrupados = listaResultadoFiltros.reduce(function(acumulador, gast){
+    let perAgrup = gast.obtenerPeriodoAgrupacion(periodo);
+    if (acumulador.hasOwnProperty(perAgrup)){
+       acumulador[perAgrup] = acumulador[perAgrup] + gast.valor;
+    } else {     
+      acumulador[perAgrup] = gast.valor;
+    }
+     return acumulador
+  }, {});
+  return gastosAgrupados;
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -148,8 +298,11 @@ export {
   anyadirGasto,
   borrarGasto,
   calcularTotalGastos,
-  calcularBalance
+  calcularBalance,
+  filtrarGastos,
+  agruparGastos,
+
   //MODIFICACIÓN EXPORT
 };
 
-/**** PASAR TESTS npm run test2 *****/
+/**** PASAR TESTS npm run test$numero *****/
