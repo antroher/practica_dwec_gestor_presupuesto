7825dce54c9c778fda3729149a8f7a9b0b30ceea
JPeregrina <72142564+JPeregrina@users.noreply.github.com>
2021-10-26 21:51:08 +0200

    Version PC Casa

--- js/test.js
+++ js/test.js
@@ -1,23 +1,58 @@
-function filtrarGastos({fechaDesdeEntrante = undefined, fechaHastaEntrante = undefined, valorMinimoEntrante = undefined,
-    valorMaximoEntrante = undefined, descripcionContieneEntrante = undefined}) {
+// function filtrarGastos({fechaDesdeEntrante = undefined, fechaHastaEntrante = undefined, valorMinimoEntrante = undefined,
+//     valorMaximoEntrante = undefined, descripcionContieneEntrante = undefined}) {
 
-let filtro = {
-fechaDesde: (!isNaN(Date.parse(fechaDesdeEntrante))) ? Date.parse(fechaDesdeEntrante) : undefined,
-fechaHasta: (!isNaN(Date.parse(fechaHastaEntrante))) ? Date.parse(fechaHastaEntrante) : undefined,
-valorMinimo: valorMinimoEntrante,
-valorMaximo: valorMaximoEntrante,
-descripcionContiene: descripcionContieneEntrante,
-// etiquetasTiene: [...etiquetasTieneEntrante]
+// let filtro = {
+// fechaDesde: (!isNaN(Date.parse(fechaDesdeEntrante))) ? Date.parse(fechaDesdeEntrante) : undefined,
+// fechaHasta: (!isNaN(Date.parse(fechaHastaEntrante))) ? Date.parse(fechaHastaEntrante) : undefined,
+// valorMinimo: valorMinimoEntrante,
+// valorMaximo: valorMaximoEntrante,
+// descripcionContiene: descripcionContieneEntrante,
+// // etiquetasTiene: [...etiquetasTieneEntrante]
 
     
+// }
+// console.log(typeof filtro.fechaDesde);
+// console.log(typeof filtro.valorMaximo);
+// console.log(typeof filtro.valorMinimo)
+// }
+function CrearGasto(descripcionEntrante, valorEntrante = 0, fechaEntrante = Date.now(), ...etiquetasEntrante) {
+    if (valorEntrante < 0 || isNaN(valorEntrante)) {
+        valorEntrante = 0;
+    }
+    if (typeof fechaEntrante === "string") {
+        if (isNaN(Date.parse(fechaEntrante))) {
+            fechaEntrante = Date.now();
+        }
+        else {
+            fechaEntrante = Date.parse(fechaEntrante);
+        }
+    }
+    let gasto = {
+        descripcion: descripcionEntrante,
+        valor: parseFloat(valorEntrante),
+        etiquetas: [...etiquetasEntrante],
+        fecha: fechaEntrante
+    }
 }
-console.log(typeof filtro.fechaDesde);
-console.log(typeof filtro.valorMaximo);
-console.log(typeof filtro.valorMinimo)
-}
 
 
+function filtrarGastos({fechaDesde, fechaHasta, valorMinimo,
+    valorMaxima, descripcionContiene, ...etiquetasTiene}) {
 
-filtrarGastos({valorMinimoEntrante: 60})
+let filtro = {
+filtroFechaDesde: (!isNaN(Date.parse(fechaDesde))) ? Date.parse(fechaDesde) : undefined,
+filtroFechaHasta: (!isNaN(Date.parse(fechaHasta))) ? Date.parse(fechaHasta) : undefined,
+filtroValorMinimo: valorMinimo,
+filtroValorMaximo: valorMaxima,
+filtroDescripcionContiene: descripcionContiene,
+filtroEtiquetasTiene: [...etiquetasTiene]
+}}
+let gasto1 = new filtrarGastos({descripcionContiene: "Seguro", valorMinimo: 10, valorMaximo: 200} );
+let valoresFiltro = Object.values(gasto1);
+let isEmpty = false;
+valoresFiltro.forEach((valor) => {
+    if (typeof valor === undefined)
+        isEmpty = true;
+})
 
-
+console.log(isEmpty)
