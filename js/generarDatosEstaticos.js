2ceb39c333404eb4004a415a40db5717f44ddd08
Alejandro Ortiz Espinosa <alextico612@gmail.com>
2021-10-27 18:35:07 +0200

    generarDatosEstaticos terminado

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,3 +1,44 @@
 import * as gp from './js/gestionPresupuesto'
-import * as gpw from '/js/gestionPresupuestoWeb'
+import * as gpw from './js/gestionPresupuestoWeb'
 
+gp.actualizarPresupuesto(1500);
+gpw.mostrarDatoEnId(gp.mostrarPresupuesto(),"presupuesto");
+gp.anyadirDasto(gp.crearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"));
+gp.anyadirDasto(gp.crearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"));
+gp.anyadirDasto(gp.crearGasto("Bonobús", 18.60, "2020-05-26", "transporte"));
+gp.anyadirDasto(gp.crearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"));
+gp.anyadirDasto(gp.crearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"));
+gp.anyadirDasto(gp.crearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"));
+gpw.mostrarDatoEnId(gp.calcularTotalGastos(),"gastos-totales");
+gpw.mostrarDatoEnId(gp.calcularBalance(),"balance-total");
+
+let gastos = gp.listarGastos();
+gastos.forEach(g => {
+    gpw.mostrarGastoWeb("listado-gastos-completo",g);
+});
+
+let gastosF=gp.filtrarGastos({fechaDesde:"2021-09-01", fechaHasta:"2021-09-30"});
+gastosF.forEach(gf => {
+    gpw.mostrarGastoWeb("listado-gastos-filtrado-1",gf);
+});
+
+let gastosF=gp.filtrarGastos({valorMinimo:50});
+gastosF.forEach(gf => {
+    gpw.mostrarGastoWeb("listado-gastos-filtrado-2",gf);
+});
+
+let gastosF=gp.filtrarGastos({valorMinimo:200,etiquetasTiene:["seguros"]});
+gastosF.forEach(gf => {
+    gpw.mostrarGastoWeb("listado-gastos-filtrado-3",gf);
+});
+
+let gastosF=gp.filtrarGastos({valorMaximo:50,etiquetasTiene:["comida","transporte"]});
+gastosF.forEach(gf => {
+    gpw.mostrarGastoWeb("listado-gastos-filtrado-4",gf);
+});
+
+gpw.mostrarGastosAgrupadosWeb("agrupacion-dia",gp.agruparGastos("dia"),"dia");
+
+gpw.mostrarGastosAgrupadosWeb("agrupacion-mes",gp.agruparGastos("mes"),"mes");
+
+gpw.mostrarGastosAgrupadosWeb("agrupacion-anyo",gp.agruparGastos("anyo"),"anyo");
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,38 +1,46 @@
 function mostrarDatoEnId(valor, idElemento){
-    let elem= document.getElementById(idElemento);
-    elem.innerHTML=valor;
+    if(idElemento!==undefined){
+        let elem= document.getElementById(idElemento);
+        elem.innerHTML+=''+valor;
+    }
+    
 }
 
 function mostrarGastoWeb(idElemento, gasto){
-    let elem = document.getElementById(idElemento);
-    let etiq="";
-    gasto.etiquetas.forEach(e => {
-        etiq+="<span class='gasto-etiquetas-etiqueta'>\n";
-        etiq+=e+"\n";
-        etiq+="</span>\n";
-    });
-    elem.innerHTML+="<div class='gasto'>\n"+
-                    "<div class='gasto-descripcion'>"+gasto.descripcion+"</div>\n"+
-                    "<div class='gasto-fecha'>"+new Date(gasto.fecha).toLocaleDateString()+"</div>\n"+
-                    "<div class='gasto-valor'>"+gasto.valor+"</div>\n"+
-                    "<div class='gasto-etiquetas'>\n"+
-                    etiq+
-                    "</div>\n"+
-                    "</div>\n";
+    if(idElemento!==undefined){
+        let elem = document.getElementById(idElemento);
+        let etiq="<div class='gasto'>\n"+
+                "<div class='gasto-descripcion'>"+gasto.descripcion+"</div>\n"+
+                "<div class='gasto-fecha'>"+new Date(gasto.fecha).toLocaleDateString()+"</div>\n"+
+                "<div class='gasto-valor'>"+gasto.valor+"</div>\n"+
+                "<div class='gasto-etiquetas'>\n";
+        gasto.etiquetas.forEach(e => {
+            etiq+="<span class='gasto-etiquetas-etiqueta'>\n";
+            etiq+=e+"\n";
+            etiq+="</span>\n";
+        });
+        etiq+="</div>\n"+
+            "</div>\n";
+        elem.innerHTML+=etiq;
+                        
+    }
+
 }
 
 function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo){
-    let elem=document.getElementById(idElemento);
-    let cad="<div class='agrupacion'>\n"+
-            "<h1>Gastos agrupados por "+periodo+"</h1>\n";
-    for(let prop in agrup){
-        cad+="<div class='agrupacion-dato'>\n"+
-            "<span class='agrupacion-dato-clave'>"+prop+"</span>\n"+
-            "<span class='agrupacion-dato-valor'>"+agrup[prop]+"</span>\n"+
-            "</div>\n";
-    }  
-
-    elem.innerHTML=cad;
+    if(idElemento!==undefined){
+        let elem=document.getElementById(idElemento);
+        let cad="<div class='agrupacion'>\n"+
+                "<h1>Gastos agrupados por "+periodo+"</h1>\n";
+        for(let prop in agrup){
+            cad+="<div class='agrupacion-dato'>\n"+
+                "<span class='agrupacion-dato-clave'>"+prop+"</span>\n"+
+                "<span class='agrupacion-dato-valor'>"+agrup[prop]+"</span>\n"+
+                "</div>\n";
+        }  
+    
+        elem.innerHTML+=cad;
+    }
 }
 
 export   {
