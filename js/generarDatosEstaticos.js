380c206bd327af38419a90b030236d3227571354
Alejandro Ortiz Espinosa <alextico612@gmail.com>
2021-10-27 17:12:25 +0200

    metodo mostrarGastosAgrupados sin test terminado

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,3 @@
+import * as gp from './js/gestionPresupuesto'
+import * as gpw from '/js/gestionPresupuestoWeb'
+
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -9,7 +9,7 @@ function mostrarGastoWeb(idElemento, gasto){
     gasto.etiquetas.forEach(e => {
         etiq+="<span class='gasto-etiquetas-etiqueta'>\n";
         etiq+=e+"\n";
-        etiq+="</span>";
+        etiq+="</span>\n";
     });
     elem.innerHTML+="<div class='gasto'>\n"+
                     "<div class='gasto-descripcion'>"+gasto.descripcion+"</div>\n"+
@@ -18,11 +18,21 @@ function mostrarGastoWeb(idElemento, gasto){
                     "<div class='gasto-etiquetas'>\n"+
                     etiq+
                     "</div>\n"+
-                    "</div>";
+                    "</div>\n";
 }
 
 function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo){
-    
+    let elem=document.getElementById(idElemento);
+    let cad="<div class='agrupacion'>\n"+
+            "<h1>Gastos agrupados por "+periodo+"</h1>\n";
+    for(let prop in agrup){
+        cad+="<div class='agrupacion-dato'>\n"+
+            "<span class='agrupacion-dato-clave'>"+prop+"</span>\n"+
+            "<span class='agrupacion-dato-valor'>"+agrup[prop]+"</span>\n"+
+            "</div>\n";
+    }  
+
+    elem.innerHTML=cad;
 }
 
 export   {
