4355ad8a42fe9c9c41512b15daca0609fedabb50
Victor Bellver <bellver20952@iesmarenostrum.com>
2021-10-27 17:23:44 +0200

    Funcion de filtrado funciona hasta las etiquetas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -200,7 +200,7 @@ function filtrarGastos(gastoF){
         let res = Object.assign(gastos);
 
         if(typeof gastoF === 'object' && gastoF !== null && gastoF !== undefined && Object.entries(gastoF).length > 0){
-            
+
             if(gastoF.hasOwnProperty('fechaDesde') && typeof gastoF.fechaDesde === 'string'){
                 res = res.filter((aux) => {
                     return aux.fecha >= (Date.parse(gastoF.fechaDesde));
@@ -210,6 +210,18 @@ function filtrarGastos(gastoF){
             if(gastoF.hasOwnProperty('fechaHasta') && typeof gastoF.fechaHasta === 'string'){
                 res = res.filter((aux) => {
                     return aux.fecha <= (Date.parse(gastoF.fechaHasta));
+                })
+            }
+
+            if(gastoF.hasOwnProperty('valorMinimo') && typeof gastoF.valorMinimo === 'number'){
+                res = res.filter((aux) => {
+                    return aux.valor >= gastoF.valorMinimo;
+                })
+            }
+
+            if(gastoF.hasOwnProperty('valorMaximo') && typeof gastoF.valorMaximo === 'number'){
+                res = res.filter((aux) => {
+                    return aux.valor <= gastoF.valorMaximo;
                 })
             }
 
