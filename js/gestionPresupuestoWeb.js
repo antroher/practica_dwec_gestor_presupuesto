aef301023651985ae0d945125c144b9403b7bfa9
Alberto Sánchez Valero <alberto.sanchez@iesdoctorbalmis.com>
2021-10-27 19:01:25 +0200

    Añado metodo id

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,25 @@
+/*El código de este fichero hará uso de la teoría explicada en la sección Documento del tutorial de JavaScript. El fichero deberá exportar las siguientes funciones:
+
+mostrarDatoEnId
+mostrarGastoWeb
+mostrarGastosAgrupadosWeb*/ 
+'use strict'
+
+function mostrarDatoEnId(texto,idElemento)
+{
+    /**Función de dos parámetros que se encargará de escribir el valor (texto) en el elemento HTML con id idElemento indicado:
+    idElemento - Hará referencia al id del elemento HTML donde se insertará el resultado en formato texto.
+    valor - El valor a mostrar */
+    
+
+
+
+
+
+
+}
+export{
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb  
+}
