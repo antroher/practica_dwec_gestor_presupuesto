dc4ec731dee13b00ac826cc6ab9cd664c9e78ca6
Adrian Jimenez <adrianjimenez1994@gmail.com>
2021-10-28 19:50:12 +0200

    no sé qué estoy haciendo

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,9 +1,24 @@
 function mostrarDatoEnId(idElemento, valor) {
-
+    
 }
 
 function mostrarGastoWeb(idElemento, gasto) {
-
+    foreach (gasto) {
+        <div class="gasto">
+            <div class="gasto-descripcion">`${gasto.descripcion}`</div>
+            <div class="gasto-fecha">`${gasto.fecha}`</div> 
+            <div class="gasto-valor">`${gasto.valor}`</div>
+            <div class="gasto-etiquetas">
+                for (let i = 0; i < gasto.etiquetas.length; i++) {
+                    <span class="gasto-etiquetas-etiqueta">
+                        `${gasto.etiquetas[i]}`
+                    </span>
+                }
+            </div> 
+            
+            
+        </div>
+    }
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup) {
