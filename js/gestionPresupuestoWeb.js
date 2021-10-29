c30dacef22a12ec6e646aea89030e642442c0a89
Adrian Jimenez <adrianjimenez1994@gmail.com>
2021-10-29 20:17:05 +0200

    dando palos de ciego como un imbécil jaja

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,12 +1,20 @@
 function mostrarDatoEnId(idElemento, valor) {
-    
+    alert(valor);
 }
 
 function mostrarGastoWeb(idElemento, gasto) {
     foreach (gasto) {
         let div = document.createElement('div');
         div.className = gasto;
-        
+        div.innerHTML = <div class="gasto-descripcion">`${gasto.descripcion}`</div><br/>
+                        <div class="gasto-fecha">`${gasto.fecha}`</div><br/>
+                        <div class="gasto-valor">`${gasto.valor}`</div><br/>
+                        <div class="gasto-etiquetas">
+                            `for (let i = 0; i < gasto.etiquetas.length; i++)` {
+                                <span class="gasto-etiquetas-etiqueta">${gasto.etiquetas[i]}</span>
+                            }
+                        </div>       
+        // `
         // <div class="gasto">
         //     <div class="gasto-descripcion">`${gasto.descripcion}`</div>
         //     <div class="gasto-fecha">`${gasto.fecha}`</div> 
@@ -21,6 +29,7 @@ function mostrarGastoWeb(idElemento, gasto) {
             
             
         // </div>
+        // `
     }
 }
 
