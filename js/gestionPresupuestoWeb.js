150948c364274bed9002ae407441670a5d55ecc8
Adrian Jimenez <adrianjimenez1994@gmail.com>
2021-10-29 19:52:40 +0200

    al fin entiendo algo (pero poco)

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -4,20 +4,23 @@ function mostrarDatoEnId(idElemento, valor) {
 
 function mostrarGastoWeb(idElemento, gasto) {
     foreach (gasto) {
-        <div class="gasto">
-            <div class="gasto-descripcion">`${gasto.descripcion}`</div>
-            <div class="gasto-fecha">`${gasto.fecha}`</div> 
-            <div class="gasto-valor">`${gasto.valor}`</div>
-            <div class="gasto-etiquetas">
-                `for (let i = 0; i < ${gasto.etiquetas.length}; i++)` {
-                    <span class="gasto-etiquetas-etiqueta">
-                        `${gasto.etiquetas[i]}`
-                    </span>
-                }
-            </div> 
+        let div = document.createElement('div');
+        div.className = gasto;
+        
+        // <div class="gasto">
+        //     <div class="gasto-descripcion">`${gasto.descripcion}`</div>
+        //     <div class="gasto-fecha">`${gasto.fecha}`</div> 
+        //     <div class="gasto-valor">`${gasto.valor}`</div>
+        //     <div class="gasto-etiquetas">
+        //         `for (let i = 0; i < ${gasto.etiquetas.length}; i++)` {
+        //             <span class="gasto-etiquetas-etiqueta">
+        //                 `${gasto.etiquetas[i]}`
+        //             </span>
+        //         }
+        //     </div> 
             
             
-        </div>
+        // </div>
     }
 }
 
