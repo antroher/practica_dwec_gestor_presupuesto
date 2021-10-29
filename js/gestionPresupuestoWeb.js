dcbb12ae59fbd6ff825662839021512631d7da0e
Adrian Jimenez <adrianjimenez1994@gmail.com>
2021-10-29 19:40:09 +0200

    exportando funciones

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -24,3 +24,10 @@ function mostrarGastoWeb(idElemento, gasto) {
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
 
 }
+
+
+export {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
