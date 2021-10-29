d093e297aaab957d822e7ba6b6e5f563b1d581d5
Joaquin Celis Abad <celis20987@iesmarenostrum.com>
2021-10-29 14:02:25 +0200

    21-10-29 Prt1 nuevo try 1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,38 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+var presupuesto = 0;
 
 
-function actualizarPresupuesto() {
+function actualizarPresupuesto(presupuesto) {
     // TODO
+    if(isNaN(presupuesto) || presupuesto < 0){
+        console.error("Error. Número no válido");
+        return -1;
+    }else{
+        return presupuesto;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return "Tu presupuesto actual es de " + presupuesto + " €";
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion, valor) {
     // TODO
+    this.descripcion = descripcion,
+    this.valor = (valor >= 0) ? valor : 0,
+    this.mostrarGasto = function(){
+        console.log("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + " €");
+    },
+    this.actualizarDescripcion = function(desc){
+        this.descripcion = desc;
+    },
+    this.actualizarValor = function(value){
+        if(value >= 0){
+            this.valor = value;      
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
