6797e6ac046fdaa0b88952f8bde439a86490ca47
Victor Escribano <victor.escribano.sanchez@gmail.com>
2021-10-29 20:43:17 +0200

    pese AgruparGastos no supera, empiezo el ejercicio 4

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,30 @@
+'use strict'
+
+
+function mostrarDatoEnId(valor1, idElemento1)
+{
+    if (typeof valor1 === 'string')
+    {
+        valor1 = valor;
+    }
+}
+
+function mostrarGastoWeb()
+{
+
+}
+
+function mostrarGastosAgrupadosWeb()
+{
+
+}
+
+
+
+
+//********** NO TOCAR **************
+export   {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
