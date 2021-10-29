ec8025c77ba881c03971567322d36362fe93b58c
Alberto Sánchez Valero <alberto.sanchez@iesdoctorbalmis.com>
2021-10-29 15:00:25 +0200

    añado mostrardato

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -11,8 +11,31 @@ function mostrarDatoEnId(texto,idElemento)
     idElemento - Hará referencia al id del elemento HTML donde se insertará el resultado en formato texto.
     valor - El valor a mostrar */
     
+    document.getElementById(idElemento).innerHTML = texto.toString();
 
+}
 
+fuction mostrarGastoWeb()
+{
+    /*Función de dos parámetros que se encargará de añadir dentro del elemento HTML con id idElemento indicado una estructura HTML para el gasto que se pase como parámetro:
+
+idElemento - Hará referencia al id del elemento HTML donde se insertará el conjunto de estructuras HTML que se crearán para cada gasto.
+gasto - Objeto gasto
+
+<div class="gasto">
+  <div class="gasto-descripcion">DESCRIPCIÓN DEL GASTO</div>
+  <div class="gasto-fecha">FECHA DEL GASTO</div> 
+  <div class="gasto-valor">VALOR DEL GASTO</div> 
+  <div class="gasto-etiquetas">
+    <span class="gasto-etiquetas-etiqueta">
+      ETIQUETA 1
+    </span>
+    <span class="gasto-etiquetas-etiqueta">
+      ETIQUETA 2
+    </span>
+    <!-- Etcétera -->
+  </div> 
+</div>*/ 
 
 
 
