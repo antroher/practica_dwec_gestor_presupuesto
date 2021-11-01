2b288beb0edfce2dd85b1c49b4a33e5bd2b97dc7
ProgramDani <90634544+ProgramDani@users.noreply.github.com>
2021-11-01 21:20:59 +0100

    comienzo práctica 4

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,7 +1,9 @@
 /*utilidades necesarias para mostrar los datos de la aplicación*/
-
+"use strict";
 function mostrarDatoEnId(idElemento,valor){
-    
+    let Elemento = document.getElementsByTagName(idElemento); //selecciona el elemento
+    let parrafo = document.createElement("p").createTextNode(valor);//crea el elemento para el texto y el contenido dentro de dicho elemento
+    Elemento.appendChild(parrafo)//añade el contenido del texto al párrafo
 }
 
 function mostrarGastoWeb(idElemento,id){
@@ -10,7 +12,7 @@ function mostrarGastoWeb(idElemento,id){
     }
 }
 
-function mostrarGastosAgrupadosWeb(){
+function mostrarGastosAgrupadosWeb(idElemento,agrup,periodos){
 
 }
 
