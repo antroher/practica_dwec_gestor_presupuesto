0038c1d4d8c0557c22c73469ffb2b8b4759167a9
Jose Juan Cantó Soriano <josejuancanto@gmail.com>
2021-11-02 20:28:59 +0100

    prueba02

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -2,3 +2,7 @@ import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js';
 import * as gestionPresupuesto from './gestionPresupuesto.js';
 
 'use strict'
+
+gestionPresupuesto.actualizarPresupuesto(1500);
+let mipres = gestionPresupuesto.mostrarPresupuesto(); // devuelve un string
+gestionPresupuestoWeb.mostrarDatoEnId('presupuesto',mipres);
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,12 +1,13 @@
 'use strict'
 
-function mostrarDatoEnId(valor,idElemento) {
+function mostrarDatoEnId(idElemento, valor) {
 
-    let idHtml = document.cfreateElement(`${idElemento}`);
+    let elem = document.getElementById(idElemento);
+    let p = document.createElement('p');
 
-    let valorHtml = toString(valor);
+    p.textContent = valor; // para modificar parte del texto del Dom, no meter codigo html nuevo
 
-    idHtml.append("\""+valorHtml+"\"");
+    elem.appendChild(p); // añade un hijo al elemento que han pasado por ID
 }
 
 function mostrarGastoWeb(idElemento,gasto) {
