a59450e739821584fcf4c12533229369d39b16c6
Raul Molina <molina21182@iesmarenostrum.com>
2021-11-02 17:04:21 +0100

    a

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,18 @@
+
+function mostrarDatoEnId () {
+
+}
+
+function mostrarGastoWeb () {
+
+}
+
+function mostrarGastosAgrupadosWeb () {
+    
+}
+
+export {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb,
+}
