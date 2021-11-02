f03a5ac7f12e338aba5595504284ef5f9929b112
Raul Molina <molina21182@iesmarenostrum.com>
2021-11-02 20:01:54 +0100

    Update gestionPresupuestoWeb.js

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,6 +1,10 @@
 
-function mostrarDatoEnId (valor, idElemento) {
-
+function mostrarDatoEnId (idElemento, valor) {
+    let elemento = document.getElementById(idElemento);
+    let parrafo = document.createElement("p");
+    if(id == elemento) {
+        parrafo.textContent(valor)
+    }
 }
 
 function mostrarGastoWeb () {
