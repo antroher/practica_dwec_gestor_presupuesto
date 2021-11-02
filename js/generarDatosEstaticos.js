599b183b1da4f324b22192ef9c727899ec04fb7d
daniel espino gomis <daniel12058@iesmarenostrum.com>
2021-11-02 19:39:59 +0100

    1

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,6 @@
+"use strict"
+
+import * as gp from 'gestionPresupuesto.js';
+import * as gpw from 'gestiponPresupuestoWeb.js';
+
+gp.actualizarPresupuesto(1500);
---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,19 @@
+"use strict"
+
+function mostrarDatoEnId (idElemento, valor){
+
+}
+
+function mostrarGastoWeb (idElemento, listaGasto){
+
+}
+
+function mostrarGastosAgrupadosWeb (idElemento, agrup, periodo){
+
+}
+
+export {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb,
+}
