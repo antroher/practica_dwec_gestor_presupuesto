7a1003e5cfd688fe9a35ec4a21f446f573aa9e4e
daniel espino gomis <daniel12058@iesmarenostrum.com>
2021-11-02 17:15:30 +0100

    test 3 - ok
    cambio la forma de realizar la función filtrarGasto() ya que con if() el test3 lo pasa correctamente pero luego con el agrupar falla.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -106,10 +106,10 @@ function CrearGasto(descripcion, valor, fecha = Date.now(), ...etiquetas) {
                 case "anyo":
                     return formatoFecha.getFullYear();
                 case "mes":
-                    return (formatoFecha.getMonth() < 10) ? formatoFecha.getFullYear() +"-0"+ (formatoFecha.getMonth()+1) : formatoFecha.getFullYear() +"-"+ (formatoFecha.getMonth()+1);
+                    return ((formatoFecha.getMonth()+1) < 10) ? formatoFecha.getFullYear() +"-0"+ (formatoFecha.getMonth()+1) : formatoFecha.getFullYear() +"-"+ (formatoFecha.getMonth()+1);
                 case "dia":
                     return formatoFecha.getFullYear() +"-"+ (
-                        (formatoFecha.getMonth() < 10) ? "0"+(formatoFecha.getMonth()+1) : (formatoFecha.getMonth()+1)) +"-"+ (
+                        ((formatoFecha.getMonth()+1) < 10) ? "0"+(formatoFecha.getMonth()+1) : (formatoFecha.getMonth()+1)) +"-"+ (
                             (formatoFecha.getDate() < 10) ? "0"+formatoFecha.getDate() : formatoFecha.getDate());
                 default:
                     break;
@@ -161,161 +161,227 @@ function calcularBalance(){
     return presupuesto - calcularTotalGastos();
 }
 
+//*************** El test 3 lo pasa pero luego da error en agruparGastos(), con if()******************
+
+// function filtrarGastos(objetoFiltrante){
+//     let fechaD;
+//     let fechaH;
+//     let valorMin;
+//     let valorMax;
+//     let descConti;
+//     let listEtiq;
+
+//     //Asignamos los valores validos, los no validos pasan a ser undefined.
+//     if(objetoFiltrante.hasOwnProperty("fechaDesde")){ //comprueba que el objeto 'objetoFiltrante' tiene la propiedad que está definida como string.
+//         if(typeof objetoFiltrante.fechaDesde === 'string'){
+//             if(isNaN(Date.parse(objetoFiltrante.fechaDesde))){
+//                 fechaD = undefined;
+//             }
+//             else{
+//                 fechaD = Date.parse(objetoFiltrante.fechaDesde);
+//             }
+//         }
+//     } 
+
+//     if(objetoFiltrante.hasOwnProperty("fechaHasta") && typeof objetoFiltrante.fechaHasta === 'string'){
+//         if(isNaN(Date.parse(objetoFiltrante.fechaHasta))){
+//             fechaH = undefined;
+//         }
+//         else{
+//             fechaH = Date.parse(objetoFiltrante.fechaHasta);
+//         }
+//     }
+
+//     if(objetoFiltrante.hasOwnProperty("valorMinimo") && !isNaN(objetoFiltrante.valorMinimo)){
+//         valorMin = objetoFiltrante.valorMinimo;
+//     }
+//     else{
+//         valorMin = undefined;
+//     }
+
+//     if(objetoFiltrante.hasOwnProperty("valorMaximo") && !isNaN(objetoFiltrante.valorMaximo)){
+//         valorMax = objetoFiltrante.valorMaximo;
+//     }
+//     else{
+//         valorMax = undefined;
+//     }
+
+//     if(objetoFiltrante.hasOwnProperty("descripcionContiene") && typeof objetoFiltrante.descripcionContiene === 'string'){
+//         descConti = objetoFiltrante.descripcionContiene;
+//     }
+//     else{
+//         descConti = undefined;
+//     }
+
+//     if(objetoFiltrante.hasOwnProperty("etiquetasTiene")){
+//         listEtiq = [...objetoFiltrante.etiquetasTiene];
+//     }
+//     else{
+//         listEtiq = undefined;
+//     }
+
+//     //Comprobamos y filtramos
+//     let listaFiltrada = gastos.filter(function(item){
+//         let devuelve = false;
+        
+//         if(listEtiq === undefined && typeof fechaD !== 'undefined' && typeof fechaH === 'undefined'){
+//             if(item.fecha >= fechaD){
+//                 devuelve = true;
+//             }
+//         }
+
+//         if(typeof fechaD === 'undefined' && typeof fechaH !== 'undefined' && item.fecha <= fechaH) devuelve = true;
+
+//         if(typeof fechaD !== 'undefined' && typeof fechaH !== 'undefined'){
+//             if(typeof valorMax === 'undefined'){
+//                 if(item.fecha >= fechaD && item.fecha <= fechaH){
+//                     devuelve = true;
+//                 }    
+//             }
+//             if(typeof valorMax !== 'undefined'){
+//                 if(item.fecha >= fechaD && item.fecha <= fechaH && item.valor < valorMax){
+//                     devuelve = true;
+//                 } 
+//             }
+
+//         } 
+
+//         if(typeof valorMax === 'undefined' && typeof valorMin !== 'undefined' && item.valor > valorMin) devuelve = true;
+
+//         if(listEtiq === undefined && typeof valorMin === 'undefined' && typeof valorMax !== 'undefined'){
+//             if(typeof fechaD === 'undefined' || typeof fechaH == 'undefined'){
+//                 if(item.valor < valorMax){
+//                     devuelve = true;
+//                 }
+//             }
+//         } 
+
+//         if(descConti === undefined && typeof valorMin !== 'undefined' && typeof valorMax !== 'undefined' && item.valor > valorMin && item.valor < valorMax) devuelve =true;
+
+//         if(typeof descConti !== 'undefined' && valorMin !== undefined && valorMax !== undefined){
+//             if(item.descripcion.includes(descConti) && item.valor >= valorMin && item.valor <= valorMax){
+//                 devuelve = true;
+//             }
+//         } 
+
+//         if(listEtiq !== undefined){
+//             if(valorMax !== undefined && fechaH === undefined){
+//                 if(item.valor <= valorMax){
+//                     for(let i = 0; i < listEtiq.length; i++){
+//                         if(item.etiquetas.includes(listEtiq[i])){
+//                             devuelve = true;
+//                         }
+//                     }        
+//                 }
+//             }
+//             if(fechaD !== undefined){
+//                 for(let i = 0; i < listEtiq.length; i++){
+//                     if(item.etiquetas.includes(listEtiq[i])){
+//                         if(item.fecha >= fechaD){
+//                             devuelve = true;
+//                         }
+//                     }
+//                 }
+//             }
+//             if(fechaH !== undefined && valorMax !== undefined){
+//                 for(let i = 0; i < listEtiq.length; i++){
+//                     if(item.etiquetas.includes(listEtiq[i])){
+//                         if(item.fecha <= fechaH && item.valor <= valorMax){
+//                             devuelve = true;
+//                         }
+//                     }
+//                 }
+//             }
+//             if (valorMax === undefined && fechaD === undefined){
+//                 for(let i = 0; i < listEtiq.length; i++){
+//                     if(item.etiquetas.includes(listEtiq[i])){
+//                         devuelve = true;
+//                     }
+//                 }
+    
+//             }
+
+//         }
+//         return devuelve;
+//     })
+
+//     if(listaFiltrada.length === 0){
+//         listaFiltrada = [...gastos]
+//     }
+//     return listaFiltrada;
+// } 
+
+function filtrarGastos(objetoDelGasto){
+    //Primera comprobacion
+    if(objetoDelGasto != undefined && objetoDelGasto !=null){  //Si el objetoDelGasto esta indefinido o es nulo que entre en el if, si no se va al else
+        let gastosFil = gastos.filter((gasto)=>{
+            if(objetoDelGasto.hasOwnProperty("fechaDesde")){
+                if(gasto.fecha < Date.parse(objetoDelGasto.fechaDesde)){
+                    return;
+                }
+            }
+            if(objetoDelGasto.hasOwnProperty("fechaHasta")){
+                if(gasto.fecha > Date.parse(objetoDelGasto.fechaHasta)){
+                    return;
+                }
+            }
+            if (objetoDelGasto.hasOwnProperty("valorMaximo")) {
+                if (gasto.valor > objetoDelGasto.valorMaximo) {
+                  return;
+                }
+              }
+            if(objetoDelGasto.hasOwnProperty("valorMinimo")){
+                if(gasto.valor < objetoDelGasto.valorMinimo){
+                    return;
+                }
+            }
+            if (objetoDelGasto.hasOwnProperty("descripcionContiene")) {
+
+                if (!gasto.descripcion.includes(objetoDelGasto.descripcionContiene))
+                        return;
+
+            }
+            if(objetoDelGasto.hasOwnProperty("etiquetasTiene")){
+                if(objetoDelGasto.etiquetasTiene.length != 0){
+                    let devu =false;
+
+                    for(let descrip of objetoDelGasto.etiquetasTiene){
+                        if(gasto.etiquetas.includes(descrip)){
+                            devu = true;
+                        }
+                    }
+                    if(!devu){
+                        return;
+                    }
+                }
+            }
+            return gasto;
+        });
+        return gastosFil;
+    }
+    else{
+        return gastos;
+    }
+}
+
 //*****Practica 3*****
-function filtrarGastos(objetoFiltrante){
-    let fechaD;
-    let fechaH;
-    let valorMin;
-    let valorMax;
-    let descConti;
-    let listEtiq;
+function agruparGastos(periodo = "mes", etiquetas = [],fechaDes,fechaHas) {
+    let resFil =  filtrarGastos({fechaDesde: fechaDes, fechaHasta: fechaHas, etiquetasTiene: etiquetas});
+    //console.log(resFil)
+    let gastAgrup = resFil.reduce(function(acumular,gasto){
+        let obtPeriodo = gasto.obtenerPeriodoAgrupacion(periodo);
 
-    //Asignamos los valores validos, los no validos pasan a ser undefined.
-    if(objetoFiltrante.hasOwnProperty("fechaDesde")){ //comprueba que el objeto 'objetoFiltrante' tiene la propiedad que está definida como string.
-        if(typeof objetoFiltrante.fechaDesde === 'string'){
-            if(isNaN(Date.parse(objetoFiltrante.fechaDesde))){
-                fechaD = undefined;
-            }
-            else{
-                fechaD = Date.parse(objetoFiltrante.fechaDesde);
-            }
-        }
-    } 
-
-    if(objetoFiltrante.hasOwnProperty("fechaHasta") && typeof objetoFiltrante.fechaHasta === 'string'){
-        if(isNaN(Date.parse(objetoFiltrante.fechaHasta))){
-            fechaH = undefined;
+        if(acumular.hasOwnProperty(obtPeriodo))
+        {
+            acumular[obtPeriodo] += gasto.valor;
         }
         else{
-            fechaH = Date.parse(objetoFiltrante.fechaHasta);
+            acumular[obtPeriodo] = gasto.valor;
         }
-    }
+        return acumular;
+    },{});
 
-    if(objetoFiltrante.hasOwnProperty("valorMinimo") && !isNaN(objetoFiltrante.valorMinimo)){
-        valorMin = objetoFiltrante.valorMinimo;
-    }
-    else{
-        valorMin = undefined;
-    }
-
-    if(objetoFiltrante.hasOwnProperty("valorMaximo") && !isNaN(objetoFiltrante.valorMaximo)){
-        valorMax = objetoFiltrante.valorMaximo;
-    }
-    else{
-        valorMax = undefined;
-    }
-
-    if(objetoFiltrante.hasOwnProperty("descripcionContiene") && typeof objetoFiltrante.descripcionContiene === 'string'){
-        descConti = objetoFiltrante.descripcionContiene;
-    }
-    else{
-        descConti = undefined;
-    }
-
-    if(objetoFiltrante.hasOwnProperty("etiquetasTiene")){
-        listEtiq = [...objetoFiltrante.etiquetasTiene];
-    }
-    else{
-        listEtiq = undefined;
-    }
-
-
-
-
-
-    //Comprobamos y filtramos
-    let listaFiltrada = gastos.filter(function(item){
-        let devuelve = false;
-        
-        if(listEtiq === undefined && typeof fechaD !== 'undefined' && typeof fechaH === 'undefined'){
-            if(item.fecha >= fechaD){
-                devuelve = true;
-            }
-        }
-
-        if(typeof fechaD === 'undefined' && typeof fechaH !== 'undefined' && item.fecha <= fechaH) devuelve = true;
-
-        if(typeof fechaD !== 'undefined' && typeof fechaH !== 'undefined'){
-            if(typeof valorMax === 'undefined'){
-                if(item.fecha >= fechaD && item.fecha <= fechaH){
-                    devuelve = true;
-                }    
-            }
-            if(typeof valorMax !== 'undefined'){
-                if(item.fecha >= fechaD && item.fecha <= fechaH && item.valor < valorMax){
-                    devuelve = true;
-                } 
-            }
-
-        } 
-
-        if(typeof valorMax === 'undefined' && typeof valorMin !== 'undefined' && item.valor > valorMin) devuelve = true;
-
-        if(listEtiq === undefined && typeof valorMin === 'undefined' && typeof valorMax !== 'undefined'){
-            if(typeof fechaD === 'undefined' || typeof fechaH == 'undefined'){
-                if(item.valor < valorMax){
-                    devuelve = true;
-                }
-            }
-        } 
-
-        if(descConti === undefined && typeof valorMin !== 'undefined' && typeof valorMax !== 'undefined' && item.valor > valorMin && item.valor < valorMax) devuelve =true;
-
-        if(typeof descConti !== 'undefined' && valorMin !== undefined && valorMax !== undefined){
-            if(item.descripcion.includes(descConti) && item.valor >= valorMin && item.valor <= valorMax){
-                devuelve = true;
-            }
-        } 
-
-        if(listEtiq !== undefined){
-            if(valorMax !== undefined && fechaH === undefined){
-                if(item.valor <= valorMax){
-                    for(let i = 0; i < listEtiq.length; i++){
-                        if(item.etiquetas.includes(listEtiq[i])){
-                            devuelve = true;
-                        }
-                    }        
-                }
-            }
-            if(fechaD !== undefined){
-                for(let i = 0; i < listEtiq.length; i++){
-                    if(item.etiquetas.includes(listEtiq[i])){
-                        if(item.fecha >= fechaD){
-                            devuelve = true;
-                        }
-                    }
-                }
-            }
-            if(fechaH !== undefined && valorMax !== undefined){
-                for(let i = 0; i < listEtiq.length; i++){
-                    if(item.etiquetas.includes(listEtiq[i])){
-                        if(item.fecha <= fechaH && item.valor <= valorMax){
-                            devuelve = true;
-                        }
-                    }
-                }
-            }
-            if (valorMax === undefined && fechaD === undefined){
-                for(let i = 0; i < listEtiq.length; i++){
-                    if(item.etiquetas.includes(listEtiq[i])){
-                        devuelve = true;
-                    }
-                }
-    
-            }
-
-        }
-        return devuelve;
-    })
-
-    if(listaFiltrada.length === 0){
-        listaFiltrada = [...gastos]
-    }
-    return listaFiltrada;
-}
-//*****Practica 3*****
-function agruparGastos(){
-
+    return gastAgrup;
 }
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
