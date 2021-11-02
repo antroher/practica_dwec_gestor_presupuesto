dba565dd0466e361b8719e819409d61087100c31
Ester Bonmati <bonmati20964@iesmarenostrum.com>
2021-11-02 19:04:50 +0100

    2021/11/02_19:04

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -9,31 +9,35 @@ function mostrarDatoEnId (idElemento, valor)
   //añade el valor el contenido de parrafo al Elemento, es decir, al HTML
   elemento.appenChild(parrafo);
 }
-/*Función de dos parámetros que se encargará de añadir dentro del elemento HTML con id idElemento indicado una estructura HTML para el gasto que se pase como parámetro:
 
-idElemento - Hará referencia al id del elemento HTML donde se insertará el conjunto de estructuras HTML que se crearán para cada gasto.
-gasto - Objeto gasto
-Así, para un determinado gasto se creará una estructura como la siguiente:
-
-<div class="gasto">
-  <div class="gasto-descripcion">DESCRIPCIÓN DEL GASTO</div>
-  <div class="gasto-fecha">FECHA DEL GASTO</div> 
-  <div class="gasto-valor">VALOR DEL GASTO</div> 
-  <div class="gasto-etiquetas">
-    <span class="gasto-etiquetas-etiqueta">
-      ETIQUETA 1
-    </span>
-    <span class="gasto-etiquetas-etiqueta">
-      ETIQUETA 2
-    </span>
-    <!-- Etcétera -->
-  </div> 
-</div>
-Donde se sustituirán los textos por los datos del gasto correspondiente.*/ 
-function mostrarGastoWeb(idElemento, )/*HAY Q PASARLE UN ARRAY DE GASTO*/ 
+function mostrarGastoWeb(idElemento, gastos )/*HAY Q PASARLE UN ARRAY DE GASTO*/ 
 {
+  let elemento = document.getElementById(idElemento);
+  let divEtiquetas = "";
 
+  for (let i of gastos.etiquetas)
+  { 
+    //guardamos enuna variable todas las etiquetas 
+    divEtiquetas += `
+    <span class="gasto-etiquetas-etiqueta">
+        ${i}
+    </span>
+    `
+  }
+  //modificamos el elemento html
+  elemento.innerHTML += `
+  <div class="gasto">
+  <div class="gasto-descripcion">${gastos.descripcion}</div>
+  <div class="gasto-fecha">${gastos.fecha}</div>
+  <div class="gasto-valor">${gastos.valor}</div>
+  <div class="gasto-etiquetas">
+  ${divEtiquetas}
+  </div>
+  </div>
+  `;
 }
+
+
 /*Función mostrarGastosAgrupadosWeb
 Función de tres parámetros que se encargará de crear dentro del elemento HTML con id idElemento indicado una estructura HTML para el objeto 
 agrup que se pase como parámetro:
