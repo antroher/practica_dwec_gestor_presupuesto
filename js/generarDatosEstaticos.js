273aa680368b056c14dee9de3256612c6d80a6c9
Ismael Feliu <feliu21025@iesmaernostrum.com>
2021-11-02 18:46:50 +0100

    Probando funciones

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,6 @@
+import * as GestPresWeb from './gestionPresupuestoWeb.js';
+import * as GestPres from './gestionPresupuesto.js';
+
+GestPres.actualizarPresupuesto(1500);
+
+GestPres.CrearGasto("Compra carne", 23.44, '2021-10-06', 'casa', 'comida');
---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,18 @@
+function mostrarDatoEnId(idElemento, valor){
+    var elem = document.getElementById(idElemento)
+    elem.innerHTML(valor);
+}
+
+function mostrarGastoWeb(){
+
+}
+
+function mostrarGastosAgrupadosWeb(){
+
+}
+
+export   {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
