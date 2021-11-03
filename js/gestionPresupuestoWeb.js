a64a61294f67ff07c528220803a6ca73391de641
Carlos Ramos Mirete <ramos13935@iesmarenostrum.com>
2021-11-03 15:49:10 +0100

    CRM Test correctos Interaccion con HTML 3/11/2021

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,36 +1,37 @@
 //Este fichero contendrá las utilidades necesarias para mostrar los datos de la aplicación en la página interaccionHTML.html
 "use strict";
-import * as gestionPresupuesto from "./gestionPresupuesto.js";
-import * as gestionPresupuestoWeb from "./generarDatosEstaticos";
 
 //Escribe el valor en el elemento HTML con id indicado
 function mostrarDatoEnId(idElemento, valor) {
-   /* let element = document.getElementById(idElemento);
+    /*** Varias formas de hacerlo ***/
+    //Forma 1
+    document.getElementById(idElemento).innerHTML = valor;
+
+    //Forma 2
+    
+    /*let element = document.getElementById(idElemento);
     let p = document.createElement("p");
-    p.textContent = valor;
+    p.textContent = valor;  
     element.appendChild(p);*/
 
-    document.getElementById(idElemento).innerHTML = "<p>" + valor + "</p>";
-    // let test = document.querySelector(idElemento);
-    //test.innerHTML = valor ;
 }
 
 //Función de dos parámetros que se encargará de añadir dentro del elemento HTML con id indicado una estructura HTML para el gasto pasado
 function mostrarGastoWeb(idElemento, gasto) {
     let textoHTML =
-        `<div>
+        `
 <div class="gasto">
   <div class="gasto-descripcion">${gasto.descripcion}</div>
   <div class="gasto-fecha">${gasto.fecha}</div> 
   <div class="gasto-valor">${gasto.valor}</div> 
   <div class="gasto-etiquetas">
 `;
-    for (let et in gasto.etiquetas) {
+    for (let et of gasto.etiquetas) {
         textoHTML += '<span class="gasto-etiquetas-etiqueta">' + et + '</span>'
     }
-    textoHTML += ' </div> </div> </div>'
+    textoHTML += ' </div> </div>'
 
-    document.getElementById(idElemento).innerHTML = textoHTML;
+    document.getElementById(idElemento).innerHTML += textoHTML;
 }
 //Función de tres parámetros que se encargará de crear dentro del elemento HTML con id indicado una estructura HTML para el objeto que se pase como parámetro
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
@@ -40,7 +41,7 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
     <div class="agrupacion">
         <h1>Gastos agrupados por ${periodo}</h1>
     `;
-    for (let propiedad of agrup) {
+    for (let propiedad in agrup) {
         textoHTML +=
          `
         <div class="agrupacion-dato">
