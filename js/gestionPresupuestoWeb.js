a53424520c3d1e7e11b05e66f2d254ddabd11905
David Ortiz <ortiz21983@iesmarenostrum.com>
2021-11-03 17:12:33 +0100

    Test 4 pasados

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -6,31 +6,33 @@ function mostrarDatoEnId(idElemento, valor){
 
 function mostrarGastoWeb(idElemento, gasto){
     let elemento = document.getElementById(idElemento);
-    elemento.innerHTML += 
+    let html = 
     "<div class='gasto'>\n" +
     "<div class='gasto-descripcion'>" + gasto.descripcion + "</div>\n" +
     "<div class='gasto-fecha'>" + new Date(gasto.fecha).toLocaleDateString() + "</div>\n" + 
     "<div class='gasto-valor'>" + gasto.valor + "</div>\n" + 
     "<div class='gasto-etiquetas'>\n";
     gasto.etiquetas.forEach(e => {
-        elemento.innerHTML += "<span class='gasto-etiquetas-etiqueta'>\n" + e + "\n</span>\n"
+        html += "<span class='gasto-etiquetas-etiqueta'>\n" + e + "\n</span>\n"
     });
-    elemento.innerHTML += "</div>\n</div>\n"
+    html += "</div>\n</div>\n";
+    elemento.innerHTML += html;
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
     let elemento = document.getElementById(idElemento);
-    elemento.innerHTML += 
+    let html= 
     "<div class='agrupacion'>\n" + 
-    "<h1>Gastos agrupados por " + periodo + "</h1>\n"
+    "<h1>Gastos agrupados por " + periodo + "</h1>\n";
     for(let prop in agrup){
-        elemento.innerHTML += 
+        html += 
         "<div class='agrupacion-dato'>\n" +
         "<span class='agrupacion-dato-clave'>" + prop + "</span>\n" +
         "<span class='agrupacion-dato-valor'>" + agrup[prop] + "</span>\n"+
         "</div>\n";
     }
-    //elemento.innerHTML += "</div>\n";
+    html += "</div>\n";
+    elemento.innerHTML += html;
 }
 
 export {
