48dffb752ac09471465353d5cfde61cbf1d40ce3
Ricardo Hernandez <hernandez16437@iesmarenostrum.com>
2021-11-04 20:10:42 +0100

    terminado mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -5,7 +5,7 @@ function mostrarDatoEnId(idElemento, valor) {
     let p = document.createElement("p");
     let elemento = document.getElementById(idElemento);
 
-    div.textContent = valor;
+    p.textContent = valor;
     elemento.appendChild(p);
 
 }
@@ -14,7 +14,23 @@ function mostrarGastoWeb(idElemento, valor) {
     
     let elemento = document.getElementById(idElemento);
 
-    for(){
+    for(let i = 0; i < gastos.length; i++) {
+
+        let etiquetas = "";
+
+        for(let j = 0; j < gasto.etiquetas; j++) {
+
+            etiquetas += `<span class="gasto-etiquetas-etiqueta"> ${j} </span>`;
+
+        }
+
+        elemento.innerHTML +=
+        `<div class="gasto"
+            <div class="gasto-descripcion">${gasto.descripcion}</div>
+            <div class="gasto-fecha">${gasto.fecha}</div>
+            <div class="gasto-valor">${gasto.valor}</div>
+            <div class="gasto-etiquetas">${data}</div>
+        `;
 
     }
     
