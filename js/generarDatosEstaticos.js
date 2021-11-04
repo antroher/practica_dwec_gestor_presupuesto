cbeb67deceffc67f7ad9c48b00cc5175329da917
Ismael Feliu <feliu21025@iesmaernostrum.com>
2021-11-04 19:37:59 +0100

    mostrarDatoenID completado :o

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -2,5 +2,15 @@ import * as GestPresWeb from './gestionPresupuestoWeb.js';
 import * as GestPres from './gestionPresupuesto.js';
 
 GestPres.actualizarPresupuesto(1500);
+GestPres.mostrarPresupuesto(mostrarDatoEnId('presupuesto'));
 
-GestPres.CrearGasto("Compra carne", 23.44, '2021-10-06', 'casa', 'comida');
+GestPres.anyadirGasto(GestPres.CrearGasto("Compra carne", 23.44, '2021-10-06', 'casa', 'comida'));
+GestPres.anyadirGasto(GestPres.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"));
+GestPres.anyadirGasto(GestPres.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte"));
+GestPres.anyadirGasto(GestPres.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"));
+GestPres.anyadirGasto(GestPres.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"));
+GestPres.anyadirGasto(GestPres.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"));
+
+GestPres.calcularTotalGastos(mostrarDatoEnId('gastos-totales'));
+GestPres.calcularBalance(mostrarDatoEnId('balance-total'));
+GestPres.listarGastos(mostrarDatoEnId('listado-gastos-completo'));
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,9 +1,27 @@
 function mostrarDatoEnId(idElemento, valor){
-    var elem = document.getElementById(idElemento)
-    elem.innerHTML(valor);
+    var elem = document.getElementById(idElemento);
+    let p = document.createElement('p');
+    p.innerHTML = valor;
+    elem.appendChild(p);
 }
 
-function mostrarGastoWeb(){
+function mostrarGastoWeb(idElemento, gasto){
+    var elem = document.getElementById(idElemento);
+
+    elem.innerHTML = `<div class="gasto">
+        <div class="gasto-descripcion">${gasto.descripcion}</div>
+        <div class="gasto-fecha">${gasto.fecha}</div> 
+        <div class="gasto-valor">${gasto.valor}</div> 
+        <div class="gasto-etiquetas">
+            <span class="gasto-etiquetas-etiqueta">
+                 
+            </span>
+            <span class="gasto-etiquetas-etiqueta">
+                ETIQUETA 2
+            </span>
+      <!-- Etcétera -->
+        </div> 
+    </div>"`
 
 }
 
