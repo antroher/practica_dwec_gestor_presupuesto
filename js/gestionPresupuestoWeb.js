fe6c1c4ef96ddd76b2e1f5cbe1426db6455a2a9a
Robert Hernández <mail.roberthernandez@gmail.com>
2021-11-04 12:18:53 +0100

    04112021_12:18

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,54 @@
+"use strict";
+
+function mostrarDatoEnId(idElemento, valor) {
+    let elemento = document.getElementById(idElemento);
+    let p = document.createElement("p");
+    p.textContent = valor;
+    elemento.appendChild(p);
+}
+
+function mostrarGastoWeb(idElemento, gastos) {
+    let elemento = document.getElementById(idElemento);
+    for (let gasto of gastos) {
+        let data = "";
+        for (let i of gasto.etiquetas) {
+            data +=
+            `<div class="gasto-etiquetas-etiqueta">
+            <span class="gasto-etiquetas-etiqueta">
+                ${i}
+            </div>
+            </span>`
+        }
+        elemento.innerHTML += 
+        `<div class="gasto">
+            <div class="gasto-descripcion">${gasto.descripcion}</div>
+            <div class="gasto-fecha">${gasto.fecha}</div> 
+            <div class="gasto-valor">${gasto.valor}</div> 
+            <div class="gasto-etiquetas">
+            ${data}`;
+    }
+}
+
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+    const elemento = document.getElementById(idElemento);
+    let data = ""
+    for (let [key, value] of Object.entries(agrup)) {
+        data += 
+        `<div class="agrupacion-dato">
+            <div class="agrupacion-dato-clave">${key}</div>
+            <div class="agrupacion-dato-valor">${value}</div>
+            <span class="agrupacion-dato-clave">${key}</span>
+            <span class="agrupacion-dato-valor">${value}</span>
+        </div>`
+    };
+    elemento.innerHTML += 
+    `<div class="agrupacion">
+        <h1>Gastos agrupados por ${periodo}</h1>
+        ${data}`
+}
+
+export {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
