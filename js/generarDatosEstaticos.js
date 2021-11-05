57ba84150bacbff817ccffe1c69d8b19e0e651b6
Robert Hernández <mail.roberthernandez@gmail.com>
2021-11-05 09:52:39 +0100

    05112021_9:52

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -7,12 +7,12 @@ GastosGen.actualizarPresupuesto(1500);
 
 GastosGenWeb.mostrarDatoEnId("presupuesto", GastosGen.mostrarPresupuesto());
 
- GastosGen.anyadirGasto(GastosGen.crearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"));
- GastosGen.anyadirGasto(GastosGen.crearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"));
- GastosGen.anyadirGasto(GastosGen.crearGasto("Bonobús", 18.60, "2020-05-26", "transporte"));
- GastosGen.anyadirGasto(GastosGen.crearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"));
- GastosGen.anyadirGasto(GastosGen.crearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"));
- GastosGen.anyadirGasto(GastosGen.crearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"));
+GastosGen.anyadirGasto(GastosGen.crearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"));
+GastosGen.anyadirGasto(GastosGen.crearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"));
+GastosGen.anyadirGasto(GastosGen.crearGasto("Bonobús", 18.60, "2020-05-26", "transporte"));
+GastosGen.anyadirGasto(GastosGen.crearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"));
+GastosGen.anyadirGasto(GastosGen.crearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"));
+GastosGen.anyadirGasto(GastosGen.crearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"));
 
 GastosGenWeb.mostrarDatoEnId("gastos-totales", GastosGen.calcularTotalGastos());
 GastosGenWeb.mostrarDatoEnId("balance-total", GastosGen.calcularBalance());
@@ -22,14 +22,14 @@ GastosGenWeb.mostrarGastoWeb("listado-gastos-filtrado-2", GastosGen.filtrarGasto
 GastosGenWeb.mostrarGastoWeb("listado-gastos-filtrado-3", GastosGen.filtrarGastos({valorMinimo: 200, etiquetasTiene:["seguros"]}))
 GastosGenWeb.mostrarGastoWeb("listado-gastos-filtrado-4", GastosGen.filtrarGastos({valorMaximo: 50, etiquetasTiene:["comida","transporte"]}));
 
-let gastoAgrupado1 = GastosGen.agruparGastos("día")
-GastosGen.agruparGastos("agrupacion-dia",gastoAgrupado1,"día")
-GastosGenWeb.mostrarGastosAgrupadosWeb("agrupacion-dia",gastoAgrupado1,"día")
+let gastoAgrupado1 = GastosGen.agruparGastos("día");
+GastosGen.agruparGastos("agrupacion-dia",gastoAgrupado1,"día");
+GastosGenWeb.mostrarGastosAgrupadosWeb("agrupacion-dia",gastoAgrupado1,"día");
 
-let gastoAgrupado2 = GastosGen.agruparGastos("mes")
-GastosGen.agruparGastos("agrupacion-mes",gastoAgrupado2,"mes")
-GastosGenWeb.mostrarGastosAgrupadosWeb("agrupacion-mes",gastoAgrupado2,"mes")
+let gastoAgrupado2 = GastosGen.agruparGastos("mes");
+GastosGen.agruparGastos("agrupacion-mes",gastoAgrupado2,"mes");
+GastosGenWeb.mostrarGastosAgrupadosWeb("agrupacion-mes",gastoAgrupado2,"mes");
 
-let gastoAgrupado3 = GastosGen.agruparGastos("anyo")
-GastosGen.agruparGastos("agrupacion-mes",gastoAgrupado3,"anyo")
-GastosGenWeb.mostrarGastosAgrupadosWeb("agrupacion-anyo",gastoAgrupado3,"año") 
+let gastoAgrupado3 = GastosGen.agruparGastos("anyo");
+GastosGen.agruparGastos("agrupacion-mes",gastoAgrupado3,"anyo");
+GastosGenWeb.mostrarGastosAgrupadosWeb("agrupacion-anyo",gastoAgrupado3,"año");
