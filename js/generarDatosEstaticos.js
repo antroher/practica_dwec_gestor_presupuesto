bd3e926e524c4eddfcefcc2b92b2e613f815e91a
daniel espino gomis <daniel12058@iesmarenostrum.com>
2021-11-05 20:36:37 +0100

    perfecto

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -17,6 +17,19 @@ gesPreWeb.mostrarDatoEnId("presupuesto",gesPre.mostrarPresupuesto());
 gesPreWeb.mostrarDatoEnId("gastos-totales", gesPre.calcularTotalGastos());
 gesPreWeb.mostrarDatoEnId("balance-total",gesPre.calcularBalance());
 
-//Listado de Gastos
 gesPreWeb.mostrarGastoWeb("listado-gastos-completo", gesPre.listarGastos())
 
+gesPreWeb.mostrarGastoWeb("listado-gastos-filtrado-1",gesPre.filtrarGastos({fechaDesde: "2021-09-01", fechaHasta: "2021-09-30"}))
+gesPreWeb.mostrarGastoWeb("listado-gastos-filtrado-2",gesPre.filtrarGastos({valorMinimo:50}))
+gesPreWeb.mostrarGastoWeb("listado-gastos-filtrado-3",gesPre.filtrarGastos({valorMinimo: 200, etiquetasTiene:["seguros"]}))
+gesPreWeb.mostrarGastoWeb("listado-gastos-filtrado-4", gesPre.filtrarGastos({valorMaximo: 50, etiquetasTiene:["comida","transporte"]}));
+
+let gastoAgrup1 = gesPre.agruparGastos("dia")
+gesPreWeb.mostrarGastosAgrupadosWeb("agrupacion-dia",gastoAgrup1,"día")
+
+let gastoAgrup2 = gesPre.agruparGastos("mes")
+gesPreWeb.mostrarGastosAgrupadosWeb("agrupacion-mes",gastoAgrup2,"mes")
+
+let gastoAgrup3 = gesPre.agruparGastos("anyo")
+
+gesPreWeb.mostrarGastosAgrupadosWeb("agrupacion-anyo",gastoAgrup3,"año")
