d31f4c96521f04c393161fccfbfed72d3e169fc2
Ricardo Hernandez <hernandez16437@iesmarenostrum.com>
2021-11-05 20:29:59 +0100

    practica 4 terminada

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,70 +1,59 @@
 import * as gestionPresupuesto from './gestionPresupuesto.js';
 
+//Para iterar sobre un collection del node usar for...of
+
+
 function mostrarDatoEnId(idElemento, valor) {
-
-    let p = document.createElement("p");
     let elemento = document.getElementById(idElemento);
-
+    let p = document.createElement("p");
     p.textContent = valor;
     elemento.appendChild(p);
-
 }
 
 function mostrarGastoWeb(idElemento, gastos) {
-    
     let elemento = document.getElementById(idElemento);
-
-    for(let gasto of gastos) {
-
-        let etiquetas = "";
-
-        for(let etiquetaObj of gasto.etiquetas) {
-
-            etiquetas += `<span class="gasto-etiquetas-etiqueta"> ${etiquetaObj} </span>`;
-
+    for (let gasto of gastos) {
+        let data = "";
+        for (let i of gasto.etiquetas) {
+            data += `
+            <span class="gasto-etiquetas-etiqueta">
+                ${i}
+            </span>`
         }
-
-        elemento.innerHTML +=
-        `<div class="gasto"
+        elemento.innerHTML += 
+        `<div class="gasto">
             <div class="gasto-descripcion">${gasto.descripcion}</div>
-            <div class="gasto-fecha">${gasto.fecha}</div>
-            <div class="gasto-valor">${gasto.valor}</div>
-            <div class="gasto-etiquetas">${etiquetas}</div>
-        </div>
-        `;
+            <div class="gasto-fecha">${gasto.fecha}</div> 
+            <div class="gasto-valor">${gasto.valor}</div> 
+            <div class="gasto-etiquetas">
+            ${data}`
 
     }
     
 }
 
-function mostrarGastoAgrupadosWeb(idElemento, periodo, agrup) {
-
-    let elemento = document.getElementById(idElemento);
-
-    let codeHTML = "";
-
-    for(let [key, value] of Object.entries(agrup)) {
- 
-        codeHTML +=
-        `<div class="agrupacion-dato">
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+    const elemento = document.getElementById(idElemento);
+    let data = ""
+    for (let [key, value] of Object.entries(agrup)) {
+        data += `
+        <div class="agrupacion-dato">
             <span class="agrupacion-dato-clave">${key}</span>
             <span class="agrupacion-dato-valor">${value}</span>
-        </div>
-        `;
-
+        </div>`
     };
-    elemento.innerHTML +=
-    `<div class="agrupacion">
+    elemento.innerHTML += 
+    `
+    <div class="agrupacion">
         <h1>Gastos agrupados por ${periodo}</h1>
-        ${datos}
-    </div>`;
-
+        ${data}
+    `
 }
 
 
 
-export {
+export   {
     mostrarDatoEnId,
-    mostrarGastoAgrupadosWeb,
-    mostrarGastoWeb
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
 }
