2954947a6c85f0c229559057598a31dd5160b02b
Raul Molina <molina21182@iesmarenostrum.com>
2021-11-05 19:39:41 +0100

    Update generarDatosEstaticos.js

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,4 +1,10 @@
 import * as gestionPresupuesto from './gestionPresupuesto.js';
 import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js';
 
+//Actualizar el presupuesto a 1500€ (función actualizarPresupuesto)
 gestionPresupuesto.actualizarPresupuesto(1500);
+
+//Mostrar el presupuesto en el div#presupuesto (funciones mostrarPresupuesto y mostrarDatoEnId)
+let mostrarPresupuesto = gestionPresupuesto.mostrarPresupuesto();
+gestionPresupuestoWeb.mostrarDatoEnId('presupuesto', mostrarPresupuesto);
+
