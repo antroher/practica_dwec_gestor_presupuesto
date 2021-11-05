91482be19af413565412fb3816e9c6fb7cdc10e1
JesusBonete <bonete20963@iesmarenostrum.com>
2021-11-05 18:38:51 +0100

    Hasiendo cosas

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,9 +1,9 @@
+import { mostrarGastosAgrupadosWeb, mostrarGastoWeb } from './gestionPresupuestoWeb';
 import * as metodoGastos from './js/gestionPresupuesto'
 import * as metodoWeb from './js/gestionPresupuestoWeb'
 
 metodoGastos.actualizarPresupuesto(1500);
-metodosGastos.mostrarPresupuesto();
-metodosGastos.mostrarDatoEnId();
+metodosGastos.mostrarDatoEnId("presupuesto", metodoGastos.mostrarPresupuesto());
 let g1= metodoGastos.CrearGasto("Comprar carne", 23.44,"2021-10-06", "supermercado", "casa");
 let g2= metodoGastos.CrearGasto("Comprar fruta y verdura", 14.25,"2021-09-06", "supermercado", "comida");
 let g3= metodoGastos.CrearGasto("Bonobús", 18.60,"2020-05-26", "supermercado");
@@ -16,3 +16,16 @@ metodoGastos.anyadirGastos(g3);
 metodoGastos.anyadirGastos(g4);
 metodoGastos.anyadirGastos(g5);
 metodoGastos.anyadirGastos(g6);
+
+
+metodoWeb.mostrarDatoEnId("gastos-totales", metodoGastos.calcularTotalGastos());
+metodoWeb.mostrarDatoEnId("balance-total", metodoGastos.calcularBalance());
+metodoWeb.mostrarGastoWeb("listado-gastos-completo", metodoGastos.listarGastos());
+metodoWeb.mostrarGastoWeb("listado-gastos-filtrado-1", metodoGastos.filtrarGastos());
+metodoWeb.mostrarGastoWeb("listado-gastos-filtrado-2", metodoGastos.filtrarGastos());
+
+//AQUÍ FALTAN 3 MÉTODOS AÚN. CALMA.
+
+metodoWeb.mostrarGastosAgrupadosWeb("agrupacio-dia", metodoGastos.agruparGastos());
+metodoWeb.mostrarGastosAgrupadosWeb("agrupacio-mes", metodoGastos.agruparGastos());
+metodoWeb.mostrarGastosAgrupadosWeb("agrupacio-anyo", metodoGastos.agruparGastos());
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,24 +1,33 @@
 
 
-function mostrarDatoEnId(idElemento, gasto){
-    let res = String(idElemento);
-    return res;
+function mostrarDatoEnId(idElemento, valor){
+    let elem = document.getElementById(idElemento);
+    let parrafo = document.createElement('p')
+    parrafo.textContent = valor;
+    elem.appendChild(p);
 }
 
-function mostrarGastoWeb(idElemento, gasto){
-    let texto1 = `<div class="gasto">
-    <div class="gasto-descripcion">${gasto.descrpcion} </div>
-    <div class="gasto-fecha">${gasto.fecha}</div> 
-    <div class="gasto-valor">${gasto.valor}</div> 
-    <div class="gasto-etiquetas">"`
-    for (let i = 0; i < gasto.etiquetas.length; i++) {
-        const element = array[i];
-        
+function mostrarGastoWeb(idElemento, valor){
+    if (IdElemento !== undefined) {
+        let texto1 = `<div class="gasto">
+        <div class="gasto-descripcion">${gasto.descrpcion} </div>   
+        <div class="gasto-fecha">${gasto.fecha}</div>
+        <div class="gasto-valor">${gasto.valor}</div> 
+        <div class="gasto-etiquetas">"`
+        for (let i = 0; i < gasto.etiquetas.length; i++) {
+            texto1 +=`<span class="gasto-etiquetas-etiqueta">
+            ${gasto.etiquetas[i]}
+        </span>   
+        </div> 
+        </div>`
+        }
     }
 }
 
-function mostrarGastosAgrupadosWeb(){
-    
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
+    let elem = document.getElementById(idElemento);
+    let html = `<div class="agrupacion">` 
+
 
 }
 
