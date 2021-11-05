c853a0d6065d1aa36e0471a099e4f7d77ae2bbee
Ismael Feliu <feliu21025@iesmaernostrum.com>
2021-11-05 18:48:16 +0100

    Todo ok, José Luis

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,50 +1,64 @@
+//Importar los programas
+
 import * as GestPresWeb from './gestionPresupuestoWeb.js';
 import * as GestPres from './gestionPresupuesto.js';
 
+//Actualizar el presupuesto a 1500€
 GestPres.actualizarPresupuesto(1500);
 
+
+//Mostrar el presupuesto en el div#presupuesto
 GestPresWeb.mostrarDatoEnId('presupuesto', GestPres.mostrarPresupuesto())
 
-
-GestPres.anyadirGasto(GestPres.CrearGasto("Compra carne", 23.44, '2021-10-06', 'casa', 'comida'));
+//Crear los siguientes gastos y Añadir los gastos creados
+GestPres.anyadirGasto(GestPres.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"));
 GestPres.anyadirGasto(GestPres.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"));
 GestPres.anyadirGasto(GestPres.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte"));
 GestPres.anyadirGasto(GestPres.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina"));
 GestPres.anyadirGasto(GestPres.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros"));
 GestPres.anyadirGasto(GestPres.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros"));
 
+//Mostrar los gastos totales en div#gastos-totales
 GestPresWeb.mostrarDatoEnId('gastos-totales', GestPres.calcularTotalGastos());
-GestPresWeb.mostrarDatoEnId('balance-total', GestPres.calcularBalance());
-GestPresWeb.mostrarDatoEnId('listado-gastos-completo', GestPres.listarGastos());
 
+//Mostrar el balance total en div#balance-total
+GestPresWeb.mostrarDatoEnId('balance-total', GestPres.calcularBalance());
+
+//Mostrar el listado completo de gastos en div#listado-gastos-completo
+GestPresWeb.mostrarGastoWeb('listado-gastos-completo', GestPres.listarGastos());
+
+//Mostrar el listado de gastos realizados en septiembre de 2021 en div#listado-gastos-filtrado-1
 let fecha1 = {fechaDesde: '2021-09-01', fechaHasta: '2021-09-30'};
 let Gastofiltrado1 = GestPres.filtrarGastos(fecha1);
 
 GestPresWeb.mostrarGastoWeb('listado-gastos-filtrado-1', Gastofiltrado1);
 
-let vgasto1 = {valorMinimo: '50€'};
+//Mostrar el listado de gastos de más de 50€ en div#listado-gastos-filtrado-2
+let vgasto1 = {valorMinimo: 50};
 let GastoFiltrado2 = GestPres.filtrarGastos(vgasto1);
 
 GestPresWeb.mostrarGastoWeb('listado-gastos-filtrado-2', GastoFiltrado2);
 
-let vgasto2 = {valorMinimo: '200€', etiquetasTiene: ['seguros']};
-let GastoFiltrado3 = GestPres.filtrarGastos(vgasto2);
+//Mostrar el listado de gastos de más de 200€ con etiqueta seguros en div#listado-gastos-filtrado-3
+let vgasto2 = {valorMinimo: 200, etiquetasTiene: ['seguros', '']};
+let gastoFiltrado3 = GestPres.filtrarGastos(vgasto2);
 
-GestPresWeb.mostrarGastoWeb('listado-gastos-filtrado-3', GastoFiltrado3);
+GestPresWeb.mostrarGastoWeb('listado-gastos-filtrado-3', gastoFiltrado3);
 
-let vgasto3 = {etiquetasTiene: ['comida', 'transporte'], valorMaximo: '50'};
-let GastoFiltrados4 = GestPres.filtrarGastos(vgasto3);
+//Mostrar el listado de gastos que tengan las etiquetas comida o transporte de menos de 50€ en div#listado-gastos-filtrado-4
+let vgasto3 = {valorMaximo: 50, etiquetasTiene: ['comida', 'transporte']};
+let gastoFiltrados4 = GestPres.filtrarGastos(vgasto3);
 
-GestPresWeb.mostrarGastoWeb('listado-gastos-filtrado-4', GastoFiltrados4);
+GestPresWeb.mostrarGastoWeb('listado-gastos-filtrado-4', gastoFiltrados4);
 
+//Mostrar el total de gastos agrupados por día en div#agrupacion-dia
 let dia1 = GestPres.agruparGastos("dia");
-GestPresWeb.mostrarGastosAgrupadosWeb('agrupacion-dia', dia1, "dia");
+GestPresWeb.mostrarGastosAgrupadosWeb('agrupacion-dia', dia1, "día");
 
+//Mostrar el total de gastos agrupados por mes en div#agrupacion-mes
 let mes1 = GestPres.agruparGastos("mes");
 GestPresWeb.mostrarGastosAgrupadosWeb('agrupacion-mes', mes1, "mes");
 
+//Mostrar el total de gastos agrupados por año en div#agrupacion-anyo
 let anyo1 = GestPres.agruparGastos("anyo");
-GestPresWeb.mostrarGastosAgrupadosWeb('agrupacion-anyo', anyo1, "anyo");
-
-
-
+GestPresWeb.mostrarGastosAgrupadosWeb('agrupacion-anyo', anyo1, "año");
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,37 +1,41 @@
+//Función mostrarDatoenId
+
 function mostrarDatoEnId(idElemento, valor){
     var elem = document.getElementById(idElemento);
-    let p = document.createElement('p');
-    p.textContent = valor;
-    elem.appendChild(p);
+    elem.textContent = valor;
 }
 
-function mostrarGastoWeb(idElemento, gasto){
-    var elem = document.getElementById(idElemento);
-    var data = " ";
+//Función mostrarGastoWeb
+function mostrarGastoWeb(idElemento, gastos){       
+    let elem = document.getElementById(idElemento);
 
-    for(let i of gasto.etiquetas){
-        data += 
-        `<span class="gasto-etiquetas-etiqueta">
-                 ${i}
-        </span>`
+    for(let gasto of gastos){
+        let cadena = "";
+        for(let i of gasto.etiquetas){
+            cadena += 
+            `<span class="gasto-etiquetas-etiqueta">
+                     ${i}
+            </span>`
+        }
+    
+        elem.innerHTML += `<div class="gasto">
+            <div class="gasto-descripcion">${gasto.descripcion}</div>
+            <div class="gasto-fecha">${gasto.fecha}</div> 
+            <div class="gasto-valor">${gasto.valor}</div> 
+            <div class="gasto-etiquetas">
+               ${cadena};
+            </div> 
+        </div>`
     }
-
-    elem.innerHTML = `<div class="gasto">
-        <div class="gasto-descripcion">${gasto.descripcion}</div>
-        <div class="gasto-fecha">${gasto.fecha}</div> 
-        <div class="gasto-valor">${gasto.valor}</div> 
-        <div class="gasto-etiquetas">
-           ${data};
-        </div> 
-    </div>`
 }
 
+//Función mostrarGastosAgrupadosWeb
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
     var elem = document.getElementById(idElemento);
 
-    let datos = ""
+    let elemento = ""
     for (let [clave, valor] of Object.entries(agrup)) {
-        datos += 
+        elemento += 
         `<div class="agrupacion-dato">
             <span class="agrupacion-dato-clave">${clave}</span>
             <span class="agrupacion-dato-valor">${valor}</span>
@@ -41,10 +45,12 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
     elem.innerHTML += 
     `<div class="agrupacion">
         <h1>Gastos agrupados por ${periodo}</h1>
-        ${datos}
+        ${elemento}
+    </div>
     `
 }
 
+//Los exports 
 export   {
     mostrarDatoEnId,
     mostrarGastoWeb,
