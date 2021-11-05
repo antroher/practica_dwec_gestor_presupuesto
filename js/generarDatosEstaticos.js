dc31e26ad8bf49d938bc49f3d77f0c40ad5d9c18
Jose Ramon Jimenez <jimenez21122@iesmarenostrum.com>
2021-11-05 17:29:21 +0100

    subido

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -3,4 +3,22 @@ import * as gestionPresupuesto from './gestionPresupuesto.js';
 
 gestionPresupuesto.actualizarPresupuesto(1500);
 
-gestionPresupuesto.mostrarPresupuesto();
+let mostrarPrep = gestionPresupuesto.mostrarPresupuesto();ç
+gestionPresupuestoWeb.mostrarDatoEnId('presupuesto', mostrarPrep);
+
+let gasto1 = gestionPresupuesto.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = gestionPresupuesto.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = gestionPresupuesto.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+let gasto4 = gestionPresupuesto.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = gestionPresupuesto.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = gestionPresupuesto.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+gestionPresupuesto.anyadirGasto(gasto1);
+gestionPresupuesto.anyadirGasto(gasto2);
+gestionPresupuesto.anyadirGasto(gasto3);
+gestionPresupuesto.anyadirGasto(gasto4);
+gestionPresupuesto.anyadirGasto(gasto5);
+gestionPresupuesto.anyadirGasto(gasto6);
+
+let calcularGastosTotales = gestionPresupuesto.calcularTotalGastos();
+gestionPresupuestoWeb.mostrarDatoEnId("gastos-totales", calcularGastosTotales);
