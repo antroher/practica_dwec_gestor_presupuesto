41e4a9ffd061d7c964ca2273007f7968b95c01fd
Senen19 <srodenad@gmail.com>
2021-11-09 17:08:11 +0100

    als,las,las

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -58,7 +58,7 @@ function filtrarGastos(gastosFilter) {
     //Deep clone
     //let gastosFiltrados = JSON.parse(JSON.stringify(gastos));
     let gastosFiltrados = Object.assign(gastos);
-    if (typeof gastosFilter === 'object' && gastosFilter != null && Object.entries(gastosFilter).length > 0) {
+    if (typeof gastosFilter === 'object' && gastosFilter != null && Object.entries(gastosFilter).length >= 0) {
         if (Object.hasOwn(gastosFilter, 'fechaDesde') && typeof gastosFilter.fechaDesde === 'string') {
             gastosFiltrados = gastosFiltrados.filter((x) => {
                 return x.fecha >= (Date.parse(gastosFilter.fechaDesde))
@@ -91,7 +91,7 @@ function filtrarGastos(gastosFilter) {
         }
         if (Object.hasOwn(gastosFilter, 'etiquetasTiene') && Array.isArray(gastosFilter.etiquetasTiene)) {
             gastosFiltrados = gastosFiltrados.filter((x) => {
-                for (let i = 0; i < gastosFilter.etiquetasTiene.length; i++) {
+                for (let i = 0; i <= gastosFilter.etiquetasTiene.length; i++) {
                     if (gastosFilter.etiquetasTiene.includes(x.etiquetas[i])) {
                         return true;
                     }
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,129 +1,118 @@
-"use strict";
-import * as gestionPresupuesto from "./gestionPresupuesto.js";
+import * as gestionPresupuesto from './gestionPresupuesto.js';
 
+//Para iterar sobre un collection del node usar for...of
 function mostrarDatoEnId(idElemento, valor) {
-  let identificador = document.getElementById(idElemento);
-  let textoValor = (identificador.innerHTML = valor);
-  return textoValor;
+    let elemento = document.getElementById(idElemento);
+    let p = document.createElement("p");
+    p.textContent = valor;
+    elemento.appendChild(p);
 }
 
+//aqui gasto es un array, con lo que habria que cambiarlo y meterlo todo dentro de una iteracción
 function mostrarGastoWeb(idElemento, gasto) {
-  let identificador = document.getElementById(idElemento);
+    let elemento = document.getElementById(idElemento);
+        //let GUID = crypto.randomUUID();
+        //let randomNumber = Math.floor((Math.random() * 1000) + 1);
+        let data = "";
+        for (let i of gasto.etiquetas) {
+            data += `
+            <span class="gasto-etiquetas-etiqueta">
+                ${i}
+            </span>`
+        }
+        elemento.innerHTML += 
+        `<div class="gasto">
+            <div class="gasto-descripcion">${gasto.descripcion}</div>
+            <div class="gasto-fecha">${gasto.fecha}</div> 
+            <div class="gasto-valor">${gasto.valor}</div> 
+            <div class="gasto-etiquetas">
+            ${data}
+            </div>
+            `;
+        let divGasto = document.querySelector(".gasto");
 
-  let divGasto = document.createElement("div");
-  divGasto.className = "gasto";
-  identificador.append(divGasto);
+        let buttonEdit = document.createElement("button");
+                            buttonEdit.className += 'gasto-editar'
+                            buttonEdit.textContent = "Editar";
+                            buttonEdit.type = 'button';
 
-  let gastoDescripcion = document.createElement("div");
-  gastoDescripcion.className = "gasto-descripcion";
-  gastoDescripcion.innerHTML = gasto.descripcion;
-  divGasto.append(gastoDescripcion);
+        let buttonBorr = document.createElement("button");
+                            buttonBorr.className += 'gasto-borrar'
+                            buttonBorr.textContent = "Borrar";
+                            buttonBorr.type = 'button';
 
-  let gastoFecha = document.createElement("div");
-  gastoFecha.className = "gasto-fecha";
-  let fechaNueva = new Date(gasto.fecha);
-  gastoFecha.innerHTML = fechaNueva.toLocaleString();
-  divGasto.append(gastoFecha);
-
-  let gastoValor = document.createElement("div");
-  gastoValor.className = "gasto-valor";
-  gastoValor.innerHTML = gasto.valor;
-  divGasto.append(gastoValor);
-
-  let gastoEtiquetas = document.createElement("div");
-  gastoEtiquetas.className = "gasto-etiquetas";
-  divGasto.append(gastoEtiquetas);
-
-  let nuevoObjEtiqueta = new BorrarEtiquetasHandle(); 
-  nuevoObjEtiqueta.gasto = gasto;
-
-  for (let x of gasto.etiquetas) {
-    let gastoEtiqueta = document.createElement("span");
-    gastoEtiqueta.className = "gasto-etiquetas-etiqueta";
-    gastoEtiqueta.innerHTML = x + "<br>";
-    nuevoObjEtiqueta.etiqueta = x;
-    gastoEtiquetas.append(gastoEtiqueta);
-
-  }
-
-  gastoEtiquetas.addEventListener('click',nuevoObjEtiqueta);
-  
-  let nuevoObj = new EditarHandle(); 
-  nuevoObj.gasto = gasto;
-  let buttonEdit = document.createElement('button'); 
-  buttonEdit.type = 'button'; 
-  buttonEdit.innerText = 'Editar';
-  buttonEdit.className = "gasto-editar";
-  buttonEdit.addEventListener('click',nuevoObj);
-  divGasto.append(buttonEdit);
-
-
-  let nuevoObjBorrar = new BorrarHandle(); 
-  nuevoObjBorrar.gasto = gasto;
-  let buttonBorrar = document.createElement('button'); 
-  buttonBorrar.type = 'button'; 
-  buttonBorrar.innerText = 'Borrar';
-  buttonBorrar.className = "gasto-borrar";
-  buttonBorrar.addEventListener('click',nuevoObjBorrar);
-  divGasto.append(buttonBorrar);
-
+        let nM = new EditarHandle();
+        let nB = new BorrarHandle();
+        nM.gasto = gasto;
+        nB.gasto = gasto;
+        
+        
+        buttonEdit.addEventListener('click', nM);
+        buttonBorr.addEventListener('click', nB);
+        
+        divGasto.append(buttonEdit);
+        divGasto.append(buttonBorr);
+    elemento.innerHTML += "================================================"
 }
 
- 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
-  let identificador = document.getElementById(idElemento);
-  let divAgrupacion = document.createElement("div");
-  divAgrupacion.className = "agrupacion";
-  identificador.append(divAgrupacion);
-  let tituloAgrupacion = document.createElement("h1");
-  tituloAgrupacion.innerHTML = `Gastos agrupados por ${periodo}`;
-  divAgrupacion.append(tituloAgrupacion);
-  for (let x in agrup) {
-        let agrupacionDato = document.createElement("div");
-        agrupacionDato.className = "agrupacion-dato";
-        divAgrupacion.append(agrupacionDato);
-
-        let agrupacionClave = document.createElement("span");
-        agrupacionClave.className = "agrupacion-dato-clave";
-        agrupacionClave.innerHTML = x + "<br>";
-        agrupacionDato.append(agrupacionClave);
-
-        let agrupacionValor = document.createElement("span");
-        agrupacionValor.className = "agrupacion-dato-valor";
-        let valorDecimal = agrup[x];
-        agrupacionValor.innerHTML = valorDecimal.toFixed(2) + "<br>";
-        agrupacionDato.append(agrupacionValor);
-  }
+    const elemento = document.getElementById(idElemento);
+    let data = ""
+    for (let [key, value] of Object.entries(agrup)) {
+        data += `
+        <div class="agrupacion-dato">
+            <span class="agrupacion-dato-clave">${key}</span>
+            <span class="agrupacion-dato-valor">${value}</span>
+        </div>`
+    };
+    elemento.innerHTML += 
+    `
+    <div class="agrupacion">
+        <h1>Gastos agrupados por ${periodo}</h1>
+        ${data}
+    `
 }
-
-
 
 function repintar() {
-    let mostrar = gestionPresupuesto.mostrarPresupuesto();
-    mostrarDatoEnId( "presupuesto",mostrar);
-    
-    let gastoTotal = gestionPresupuesto.calcularTotalGastos().toFixed(2);
-    mostrarDatoEnId( "gastos-totales",gastoTotal);
-    
-    let balanceTotal = gestionPresupuesto.calcularBalance().toFixed(2);
-    mostrarDatoEnId("balance-total",balanceTotal);
-    
-    let borrarDatos = document.getElementById("listado-gastos-completo").innerHTML = "";
-    
-    let matrizGasto = gestionPresupuesto.listarGastos();
-    for (const x of matrizGasto) {
-        mostrarGastoWeb("listado-gastos-completo", x);
-    }
+    //Presupuesto
+    let mostPresupuesto = gestionPresupuesto.mostrarPresupuesto();
+    mostrarDatoEnId('presupuesto', mostPresupuesto);
+
+    //Total de gastos
+    let calcularTotalGastos = gestionPresupuesto.calcularTotalGastos();
+    mostrarDatoEnId("gastos-totales", calcularTotalGastos);
+
+    //Balance actual
+    let calcularBalance = gestionPresupuesto.calcularBalance();
+    mostrarDatoEnId("balance-total", calcularBalance);
+
+    //Borrar div#listado-gastos-completo | Listado con los gastos y sus datos
+    document.getElementById("listado-gastos-completo").innerHTML = "";
+    let listaGastos = gestionPresupuesto.listarGastos();
+    mostrarGastoWeb("listado-gastos-completo", listaGastos);
 }
 
+function formatearFecha(date) {
+    let d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
 
-function actualizarPresupuestoWeb (){
-    let cambioPresupuesto = parseInt(prompt("Cual es el valor del presupuesto actualmente"));
-    gestionPresupuesto.actualizarPresupuesto(cambioPresupuesto);
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+function actualizarPresupuestoWeb() {
+    let presupuesto = parseFloat(prompt("Introduzca un presupuesto: "))
+    gestionPresupuesto.actualizarPresupuesto(presupuesto);
     repintar();
 }
 
-  function nuevoGastoWeb(){
+function nuevoGastoWeb(){
     let descripcion = prompt("Escribe la descripción del gasto");
     let valor1 = parseFloat(prompt("Escribe el valor del gasto"));
     let fecha = prompt("Escribe la fecha del gasto en formato yyyy-mm-dd");
@@ -134,43 +123,52 @@ function actualizarPresupuestoWeb (){
     repintar();
   }
 
+/* https://stackoverflow.com/questions/2230992/javascript-creating-objects-based-on-a-prototype-without-using-new-constructo*/
+
 function EditarHandle() {
-    this.handleEvent = function (e){
-    let descripcion = prompt("Escribe la nueva descripción del gasto");
-    let valor1 = parseFloat(prompt("Escribe la nueva valor del gasto"));
-    let fecha = prompt("Escribe la fecha del gasto en formato yyyy-mm-dd");
-    let etiquetas = prompt("Escribe las etiquetas del gasto separadas por ,");
-    let etiquetasArray = etiquetas.split(',');
-    this.gasto.actualizarValor(valor1);
-    this.gasto.actualizarDescripcion(descripcion);
-    this.gasto.actualizarFecha(fecha);
-    this.gasto.anyadirEtiquetas(...etiquetasArray);
-    repintar();
-   }
+    this.handleEvent = function (event){
+        let descripcion = prompt("Escribe la nueva descripción del gasto");
+        let valor1 = parseFloat(prompt("Escribe la nueva valor del gasto"));
+        let fecha = prompt("Escribe la fecha del gasto en formato yyyy-mm-dd");
+        let etiquetas = prompt("Escribe las etiquetas del gasto separadas por ,");
+        let etiquetasArray = etiquetas.split(',');
+        this.gasto.actualizarValor(valor1);
+        this.gasto.actualizarDescripcion(descripcion);
+        this.gasto.actualizarFecha(fecha);
+        this.gasto.anyadirEtiquetas(...etiquetasArray);
+        repintar();
+    }
 }
 
-  function BorrarHandle() {
-    this.handleEvent = function (e){
+function BorrarHandle() {
+    this.handleEvent = function (event){
       let number = this.gasto.id;
       gestionPresupuesto.borrarGasto(number);
       repintar();
     }
 }
 
-  function BorrarEtiquetasHandle() {
-    this.handleEvent = function (e){
+function BorrarEtiquetasHandle() {
+    this.handleEvent = function (event){
     this.gasto.borrarEtiquetas(this.etiqueta);
     repintar();
    }
 }
  
 
- 
-let botonGasto = document.getElementById("anyadirgasto");
-botonGasto.addEventListener("click",nuevoGastoWeb);
+//Botones
+const actualizarpresupuesto = document.getElementById("actualizarpresupuesto");
+const anyadirgasto = document.getElementById("anyadirgasto");
+
+const gastoBorrar = document.getElementById("gasto-borrar");
+
+//Eventos
+actualizarpresupuesto.addEventListener('click', actualizarPresupuestoWeb);
+anyadirgasto.addEventListener('click', nuevoGastoWeb);
 
 
-let botonActualizarPresupuesto = document.getElementById("actualizarpresupuesto");
-botonActualizarPresupuesto.addEventListener("click",actualizarPresupuestoWeb);
-
-export { mostrarDatoEnId, mostrarGastoWeb, mostrarGastosAgrupadosWeb };
+export   {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
