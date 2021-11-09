5743ffca23d903dac98919655533e9a64a7f1dab
gomez20728 <72142611+gomez20728@users.noreply.github.com>
2021-11-09 16:36:55 +0100

    generarDatosEstaticos Creado

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,84 @@
+
+
+import * as gestionPresupuesto from './gestionPresupuesto.js';
+import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js';
+
+
+
+
+gestionPresupuesto.actualizarPresupuesto(1500);
+
+
+let mipres = gestionPresupuesto.mostrarPresupuesto();
+gestionPresupuestoWeb.mostrarDatoEnId('presupuesto',mipres);
+
+
+let gasto1 = gestionPresupuesto.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = gestionPresupuesto.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = gestionPresupuesto.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+let gasto4 = gestionPresupuesto.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = gestionPresupuesto.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = gestionPresupuesto.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+
+
+gestionPresupuesto.anyadirGasto(gasto1);
+gestionPresupuesto.anyadirGasto(gasto2);
+gestionPresupuesto.anyadirGasto(gasto3);
+gestionPresupuesto.anyadirGasto(gasto4);
+gestionPresupuesto.anyadirGasto(gasto5);
+gestionPresupuesto.anyadirGasto(gasto6);
+
+
+
+let gastosTotales = gestionPresupuesto.calcularTotalGastos();
+gestionPresupuestoWeb.mostrarDatoEnId("gastos-totales",gastosTotales);
+
+let balanceTotal = gestionPresupuesto.calcularBalance();
+gestionPresupuestoWeb.mostrarDatoEnId("balance-total", balanceTotal); 
+
+
+let gastosListados = gestionPresupuesto.listarGastos();
+for(let elem of gastosListados)
+{
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-completo",elem);
+}
+
+
+let objeto, gastoFiltrado, etiquetas;
+objeto = {fechaDesde: "2021-09-01", fechaHasta: "2021-09-30"};
+gastoFiltrado = gestionPresupuesto.filtrarGastos(objeto);
+    for(let elem of gastoFiltrado)
+        gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-1",elem);
+
+ 
+objeto = {valorMinimo: 50};
+gastoFiltrado = gestionPresupuesto.filtrarGastos(objeto);
+    for(let elem of gastoFiltrado)
+        gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-2",elem);
+
+
+
+objeto = {valorMinimo: 200, etiquetasTiene:["seguros"]}
+
+gastoFiltrado = gestionPresupuesto.filtrarGastos(objeto);
+    for(let elem of gastoFiltrado)
+        gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-3",elem);
+
+
+objeto = {valorMaximo: 50, etiquetasTiene:["comida", "transporte"]}
+
+
+gastoFiltrado = gestionPresupuesto.filtrarGastos(objeto);
+    for(let elem of gastoFiltrado)
+        gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-4",elem);
+
+
+
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-dia", gestionPresupuesto.agruparGastos("dia"), "día");
+
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-mes", gestionPresupuesto.agruparGastos("mes"), "mes");
+
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb("agrupacion-anyo", gestionPresupuesto.agruparGastos("anyo"), "año");
+
+
