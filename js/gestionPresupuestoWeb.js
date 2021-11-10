f67daf2d0b3f2596dc68b082dcff6df476e82e5e
daniel espino gomis <daniel12058@iesmarenostrum.com>
2021-11-10 15:52:03 +0100

    3

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,53 +1,62 @@
 "use strict"
-"use strict"
+
+import * as gesPre from "./gestionPresupuesto.js";
+
+
 function mostrarDatoEnId(idElemento,valor){
     let elemento = document.getElementById(idElemento);
     elemento.innerHTML += `<p>${valor}</p>`
     
 }
     function mostrarGastoWeb(idElemento,gastos){
-        let element = document.getElementById(idElemento);
-    
-        gastos.forEach((gasto) => {
-            let etiquetas = "";
-            gasto.etiquetas.forEach((etiqueta) => {
-                etiquetas += 
-                    `<span class="gasto-etiquetas-etiqueta">
-                        ${etiqueta}
-                    </span>`;
-            });    
-    
-            element.innerHTML +=
-                `<div class="gasto">
-                    <div class="gasto-descripcion">${gasto.descripcion}</div>
-                    <div class="gasto-fecha">${gasto.fecha}</div> 
-                    <div class="gasto-valor">${gasto.valor}</div> 
-                    <div class="gasto-etiquetas">
-                        ${etiquetas}
-                    </div>
-                </div>`;
-        });
-    }
-    
-        function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo){
-            let elemento = document.getElementById(idElemento);
+    let element = document.getElementById(idElemento);
 
-            //bucle tocho
-          let gastos ="";
-            for(let prop in agrup){
-                gastos +=
-                "<div class='agrupacion-dato'>" +
-                    "<span class='agrupacion-dato-clave'>" + prop + ": </span>" +
-                    "<span class='agrupacion-dato-valor'>" + agrup[prop] + "</span>"+
-                "</div>";
-            }
+    gastos.forEach((gasto) => {
+        let etiquetas = "";
+        gasto.etiquetas.forEach((etiqueta) => {
+            etiquetas += 
+                `<span class="gasto-etiquetas-etiqueta">
+                    ${etiqueta}
+                </span>`;
+        });    
 
-            elemento.innerHTML += 
-            `<div class='agrupacion'> 
-                <h1>Gastos agrupados por ${periodo} </h1>
-                ${gastos}
+        element.innerHTML +=
+            `<div class="gasto">
+                <div class="gasto-descripcion">${gasto.descripcion}</div>
+                <div class="gasto-fecha">${gasto.fecha}</div> 
+                <div class="gasto-valor">${gasto.valor}</div> 
+                <div class="gasto-etiquetas">
+                    ${etiquetas}
+                </div>
             </div>`;
-        }
+    });
+}
+    
+function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo){
+    let elemento = document.getElementById(idElemento);
+
+    
+    let gastos ="";
+    for(let prop in agrup){
+        gastos +=
+        "<div class='agrupacion-dato'>" +
+            "<span class='agrupacion-dato-clave'>" + prop + ": </span>" +
+            "<span class='agrupacion-dato-valor'>" + agrup[prop] + "</span>"+
+        "</div>";
+    }
+
+    elemento.innerHTML += 
+    `<div class='agrupacion'> 
+        <h1>Gastos agrupados por ${periodo} </h1>
+        ${gastos}
+    </div>`;
+}
+
+function repintar(){
+    mostrarDatoEnId("presupuesto",gesPre.mostrarPresupuesto());
+    mostrarDatoEnId("gastos-totales", gesPre.calcularTotalGastos());
+    mostrarDatoEnId("balance-total",gesPre.calcularBalance());
+}
 
 
 //El export de las funciones
