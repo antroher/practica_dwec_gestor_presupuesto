10da10dd91150eb3d198ba45bf96839f50962fb4
Robert Hernández <mail.roberthernandez@gmail.com>
2021-11-14 17:21:33 +0100

    12112021_17:21

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import * as gestionPresupuesto from "./gestionPresupuesto.js";
+
 function mostrarDatoEnId(idElemento, valor) {
     let elemento = document.getElementById(idElemento);
     let p = document.createElement("p");
@@ -13,12 +15,12 @@ function mostrarGastoWeb(idElemento, gastos) {
         let data = "";
         for (let i of gasto.etiquetas) {
             data +=
-            `<span class="gasto-etiquetas-etiqueta">
+                `<span class="gasto-etiquetas-etiqueta">
                 ${i}
             </span>`
         }
-        elemento.innerHTML += 
-        `<div class="gasto">
+        elemento.innerHTML +=
+            `<div class="gasto">
             <div class="gasto-descripcion">${gasto.descripcion}</div>
             <div class="gasto-fecha">${gasto.fecha}</div> 
             <div class="gasto-valor">${gasto.valor}</div> 
@@ -33,21 +35,20 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
     let agrupacion = '';
     let elemento = document.getElementById(idElemento);
 
-    for (let [param,value] of Object.entries(agrup)) {
+    for (let [param, value] of Object.entries(agrup)) {
         agrupacion += `
         <div class="agrupacion-dato">
         <span class="agrupacion-dato-clave">${param}</span>
         <span class="agrupacion-dato-valor">${value}</span>
         </div>`
     };
-    elemento.innerHTML += 
-    `<div class="agrupacion">
+    elemento.innerHTML +=
+        `<div class="agrupacion">
       if (${periodo}==='dia')
       <h1>Gastos agrupados por día</h1>
         else
         <h1>Gastos agrupados por ${periodo}</h1>
         ${data}`
-}
 
     elemento.innerHTML += `
     <div class="agrupacion">
@@ -56,8 +57,26 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
     `
 }
 
+function repintar() {
+    mostrarDatoEnId("presupuesto", gestionPresupuesto.mostrarPresupuesto());
+    mostrarDatoEnId("gastos-totales", gestionPresupuesto.calcularTotalGastos());
+    mostrarDatoEnId("balance-total", gestionPresupuesto.calcularBalance());
+
+    document.getElementById("listado-gastos-completo").innerHTML = "";
+    mostrarGastoWeb("listado-gastos-completo", gestionPresupuesto.listarGastos());
+}
+
+function actualizarPresupuestoWeb() {
+    gestionPresupuesto.actualizarPresupuesto(parseFloat(prompt("Introduce un nuevo presupuesto:")));
+    
+    repintar();
+} 
+
+
+
 export {
     mostrarDatoEnId,
     mostrarGastoWeb,
-    mostrarGastosAgrupadosWeb
+    mostrarGastosAgrupadosWeb,
+    repintar
 }
