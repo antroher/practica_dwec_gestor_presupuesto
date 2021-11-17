a526f614928158d212eae89a4fa1631250a575ba
Noelia <pol20673@iesmarenostrum.com>
2021-11-17 17:35:20 +0100

    hoy

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -7,7 +7,6 @@ gestionPresupuesto.actualizarPresupuesto(1500);
 
 //Mostrar presupuesto.
 let mostrarPres=gestionPresupuesto.mostrarPresupuesto();
-alert(mostrarPres);
 
 gestionPresupuestoWeb.mostrarDatoEnId('presupuesto',mostrarPres);
 
@@ -45,49 +44,51 @@ for(let g of listaGastos)
 
 //Mostrar el listado de gastos realizados en septiembre de 2021
 
-let filtraGast= gestionPresupuesto.filtrarGastos({fechaDesde:"2021-09-01", fechaHasta:"2021-09-30"});
-
+listaGastos= gestionPresupuesto.filtrarGastos({fechaDesde:"2021-09-01", fechaHasta:"2021-09-30"});
+for(let gast of listaGastos)
+{
+    gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-1', gast);
+}
 
 //Mostrar el listado de gastos de más de 50€ 
 
-let listaGastos= gestionPresupuesto.listarGastos({valorMinimo: 50});
+listaGastos= gestionPresupuesto.filtrarGastos({valorMinimo: 50});
 for(let gast of listaGastos)
 {
-    gestionPresupuesto.mostrarGastoWeb('listado-gastos-filtrado-2', gast);
+    gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-2', gast);
 }
 
-let filtGastCinc= gestionPresupuesto.filtrarGastos({valorMinimo: 50});
 
 //Mostrar el listado de gastos de más de 200€ con etiqueta seguros
 
-let listaGastos= gestionPresupuesto.listarGastos({valorMinimo: 200, etiquetasTiene: ['seguros']});
-for(let gas of listaGastos)
+listaGastos= gestionPresupuesto.filtrarGastos({valorMinimo: 200, etiquetasTiene: ['seguros']});
+for(let gast of listaGastos)
 {
-    gestionPresupuesto.mostrarGastoWeb('listado-gastos-filtrado-3', gas);
+    gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-3', gast);
 }
 
 
 //Mostrar el listado de gastos que tengan las etiquetas comida o transporte de menos de 50€ 
-let listaGastos= gestionPresupuesto.listarGastos({etiquetasTiene: ['comida', 'transporte'], valorMaximo: 50});
-for(let ga of listaGastos)
+listaGastos= gestionPresupuesto.filtrarGastos({ valorMaximo: 50, etiquetasTiene: ['comida', 'transporte']});
+for(let gast of listaGastos)
 {
-    gestionPresupuesto.mostrarGastoWeb('listado-gastos-filtrado-4', ga);
+    gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-4', gast);
 }
 
 
 //Mostrar el total de gastos agrupados por día
 
-let agrup1 = gP.agruparGastos('dia');
-gPW.mostrarGastosAgrupadosWeb('agrupacion-dia', agrup1, 'día');
+let agrup1 = gestionPresupuesto.agruparGastos('dia');
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb('agrupacion-dia', agrup1, 'día');
 
 //Mostrar el total de gastos agrupados por mes
 
-let agrup2 = gP.agruparGastos('mes');
-gPW.mostrarGastosAgrupadosWeb('agrupacion-mes', agrup2, 'mes');
+let agrup2 = gestionPresupuesto.agruparGastos('mes');
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb('agrupacion-mes', agrup2, 'mes');
 
 //Mostrar el total de gastos agrupados por año
 
-let agrup3 = gP.agruparGastos('anyo');
-gPW.mostrarGastosAgrupadosWeb('agrupacion-anyo', agrup3, 'año');
+let agrup3 = gestionPresupuesto.agruparGastos('anyo');
+gestionPresupuestoWeb.mostrarGastosAgrupadosWeb('agrupacion-anyo', agrup3, 'año');
 
 //               (IMPORTANTE) ---->                FOR OF PARA ARRAYS - FOR IN PARA EL RESTO.
