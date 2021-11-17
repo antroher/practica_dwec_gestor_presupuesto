c49ad356c4563d50ec7e4db20b20adadaf640d4a
Andreu Garri <garri21066@iesmarenostrum.com>
2021-11-17 11:39:34 +0100

    Pract.5 Acabada

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,5 +1,10 @@
 "use strict";
 
+import * as gestionpre from './gestionPresupuesto.js';
+
+document.getElementById("actualizarpresupuesto").addEventListener("click", actualizarPresupuestoWeb);
+document.getElementById("anyadirgasto").addEventListener("click", nuevoGastoWeb);
+
 function mostrarDatoEnId(idElemento, valor)
 {
     let elem = document.getElementById(idElemento);
@@ -11,7 +16,7 @@ function mostrarDatoEnId(idElemento, valor)
 
 function mostrarGastoWeb(idElemento, gasto)
 {
-    let div = `<div class="gasto">
+    /*let div = `<div class="gasto">
                             <div class="gasto-descripcion"> ${gasto.descripcion} </div>
                             <div class="gasto-fecha"> ${gasto.fecha} </div>
                             <div class="gasto-valor"> ${gasto.valor} </div>
@@ -22,7 +27,73 @@ function mostrarGastoWeb(idElemento, gasto)
         
     div += `</div></div>`;
 
-    document.getElementById(idElemento).innerHTML += div;
+    document.getElementById(idElemento).innerHTML += div;*/
+
+    let ele = document.getElementById(idElemento);  
+    
+    let divG1 = document.createElement('div');
+    divG1.className = 'gasto';
+
+    let divGastoDescri = document.createElement('div');
+    divGastoDescri.className = 'gasto-descripcion'; // Le asignamos el id 'gasto-descripcion'
+    divGastoDescri.textContent = gasto.descripcion;
+    divG1.append(divGastoDescri); //divGastoDescripcion hijo de divGasto
+    
+    
+    let divGastoFech = document.createElement('div');
+    divGastoFech.className = 'gasto-fecha'; 
+    divGastoFech.textContent = new Date(gasto.fecha).toLocaleDateString();
+    divG1.append(divGastoFech); 
+
+    
+    let divGastoV1 = document.createElement('div');
+    divGastoV1.className = 'gasto-valor'; 
+    divGastoV1.textContent = gasto.valor + '';
+    divG1.append(divGastoV1); 
+
+    
+    let divGastoEtiq = document.createElement('div');
+    divGastoEtiq.className = 'gasto-etiquetas'; 
+    gasto.etiquetas.forEach(label =>
+        {
+            let spanetiq = document.createElement('span');
+            spanetiq.className = 'gasto-etiquetas-etiqueta';
+            spanetiq.textContent = label + '';
+
+            let borraEti = new BorrarEtiquetasHandle();
+            borraEti.gasto = gasto;
+            borraEti.etiqueta = label;
+            spanetiq.addEventListener('click', borraEti);
+            divGastoEtiq.append(spanetiq);            
+        }
+    );
+
+    divG1.append(divGastoEtiq); 
+
+    if(idElemento == 'listado-gastos-completo')
+    {
+        let btnEditar = document.createElement('button');
+        btnEditar.className = 'gasto-editar';
+        btnEditar.type = 'button';
+        btnEditar.textContent = 'Editar';
+
+        let editarHandle = new EditarHandle();
+        editarHandle.gasto = gasto;
+        btnEditar.addEventListener('click', editarHandle);
+        divG1.append(btnEditar);
+
+        let btnBorrar = document.createElement('button');
+        btnBorrar.className = 'gasto-borrar';
+        btnBorrar.type = 'button';
+        btnBorrar.textContent = 'Borrar';
+
+        let borrarHandle = new BorrarHandle();
+        borrarHandle.gasto = gasto;
+        btnBorrar.addEventListener('click', borrarHandle);
+        divG1.append(btnBorrar);
+    }
+    
+    ele.append(divG1);
 
 }
         
@@ -43,7 +114,98 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo)
     }
     mensaje += "</div>\n";
     elemento.innerHTML += mensaje;
+
+}
+
+function repintar(){
+    document.getElementById('presupuesto').innerHTML = '';
+    document.getElementById('balance-total').innerHTML = '';
+    document.getElementById('gastos-totales').innerHTML = '';
+    mostrarDatoEnId('presupuesto', gestionpre.mostrarPresupuesto());
+    mostrarDatoEnId('gastos-totales', gestionpre.calcularTotalGastos());
+    mostrarDatoEnId('balance-total', gestionpre.calcularBalance());
+
+    document.getElementById('listado-gastos-completo').innerHTML = '';
+    let lisgas = gestionpre.listarGastos();
+    lisgas.forEach(exp => {mostrarGastoWeb('listado-gastos-completo', exp);});
     
+    document.getElementById('listado-gastos-filtrado-1').innerHTML = '';
+    let gastosFlt = gestionpre.filtrarGastos({fechaDesde:'2021-09-01', fechaHasta:'2021-09-30'});
+    gastosFlt.forEach(gastoFiltrado => {mostrarGastoWeb('listado-gastos-filtrado-1', gastoFiltrado);});
+
+    document.getElementById('listado-gastos-filtrado-2').innerHTML = '';
+    gastosFlt = gestionpre.filtrarGastos({valorMinimo:50});
+    gastosFlt.forEach(gastoFiltrado => {mostrarGastoWeb('listado-gastos-filtrado-2', gastoFiltrado);});
+
+    document.getElementById('listado-gastos-filtrado-3').innerHTML = '';
+    gastosFlt = gestionpre.filtrarGastos({valorMinimo:200,etiquetasTiene:['seguros']});
+    gastosFlt.forEach(gastoFiltrado => {mostrarGastoWeb('listado-gastos-filtrado-3', gastoFiltrado);});
+
+    document.getElementById('listado-gastos-filtrado-4').innerHTML = '';
+    gastosFlt = gestionpre.filtrarGastos({valorMaximo:50,etiquetasTiene:['comida','transporte']});
+    gastosFlt.forEach(gastoFiltrado => {mostrarGastoWeb('listado-gastos-filtrado-4', gastoFiltrado);});
+    
+}
+
+function actualizarPresupuestoWeb(){
+    let presupuesto = prompt('Introduce un presupuesto nuevo');
+    presupuesto = parseInt(presupuesto);
+
+    gestionpre.actualizarPresupuesto(presupuesto);
+
+    repintar();
+}
+
+function nuevoGastoWeb(){
+    let descrip = prompt('Introduce la descripción del gasto: ');
+    let valor = prompt('Introduce el valor del gasto: ');
+    let fecha = prompt('Introduce la fecha del gasto: ');
+    let etiq = prompt('Introduce las etiquetas: ');
+
+    valor = parseFloat(valor);
+    etiq = etiq.split(',');
+
+    let g1 = new gestionpre.CrearGasto(descrip, valor, fecha, etiq);
+
+    gestionpre.anyadirGasto(g1);
+
+    repintar();
+}
+
+function BorrarHandle(){
+    this.handleEvent = function()
+    {
+        gestionpre.borrarGasto(this.gasto.id);
+        repintar();
+    };
+ }
+ function BorrarEtiquetasHandle(){
+    this.handleEvent = function()
+    {
+        this.gasto.borrarEtiquetas(this.etiqueta);
+        repintar();
+    };
+ }
+
+ function EditarHandle(){
+
+    this.handleEvent = function(e){
+       //Pedir datos al usuario
+       let descri = prompt("Introduce la descripción nueva: ", this.gasto.descripcion);
+       let v1 = prompt("Introduce el valor nuevo: ", this.gasto.valor);
+       let f1 = prompt("Introduce la fecha nueva: ", this.gasto.fecha);
+       let etiq = prompt("Inroduce las etiquetas nuevas: ", this.gasto.etiquetas);
+
+       v1 = parseFloat(v1);
+       etiq = etiq.split(',');
+
+       this.gasto.actualizarDescripcion(descri);
+       this.gasto.actualizarValor(v1);
+       this.gasto.actualizarFecha(f1);
+       this.gasto.anyadirEtiquetas(...etiq);
+
+       repintar();
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
@@ -52,5 +214,11 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo)
 export   {
     mostrarDatoEnId,
     mostrarGastoWeb,
-    mostrarGastosAgrupadosWeb
+    mostrarGastosAgrupadosWeb,
+    repintar,
+    actualizarPresupuestoWeb,
+    nuevoGastoWeb,
+    BorrarHandle,
+    BorrarEtiquetasHandle,
+    EditarHandle
 }
