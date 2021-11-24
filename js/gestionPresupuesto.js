f41e8b5cb1fd1473df53558ee8c92c3904d4c1a4
Joaquin Celis Abad <celis20987@iesmarenostrum.com>
2021-11-24 09:45:38 +0100

    2021-11-24 otra hora perdida gracias a git

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,51 +1,40 @@
+// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-"use strict";
-// Variables globales   -------------------------------------------------------------------------------
+// TODO: Variable global
 var presupuesto = 0;
-var gastos = [];
-var idGasto = 0;
 
-// Funciones        -----------------------------------------------------------------------------------
-function actualizarPresupuesto(value){
-    
-    if(isNaN(value) || value < 0){
+
+function actualizarPresupuesto(presupuesto) {
+    // TODO
+    if(isNaN(presupuesto) || presupuesto < 0){
         console.error("Error. Número no válido");
         return -1;
     }else{
-        presupuesto = value;
         return presupuesto;
     }
 }
 
-function mostrarPresupuesto(){
-    
+function mostrarPresupuesto() {
+    // TODO
     return "Tu presupuesto actual es de " + presupuesto + " €";
 }
 
-function CrearGasto(descripcion, valor){
-    
-    this.descripcion = descripcion;
-    this.valor = (valor >= 0) ? valor : 0;
+function CrearGasto(descripcion, valor) {
+    // TODO
+    this.descripcion = descripcion,
+    this.valor = (valor >= 0) ? valor : 0,
     this.mostrarGasto = function(){
-        return "Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + " €";
-    };
+        console.log("Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + " €");
+    },
     this.actualizarDescripcion = function(desc){
         this.descripcion = desc;
-    };
+    },
     this.actualizarValor = function(value){
         if(value >= 0){
             this.valor = value;      
         }
-    };
-    
-    
+    }
 }
-
-function listarGastos(){
-    return gastos;
-}
-
-
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
