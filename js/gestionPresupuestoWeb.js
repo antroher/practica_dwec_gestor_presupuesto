cccabfa5e163ed9c9e4a6b0b52f441374c12de58
Robert Hernández <mail.roberthernandez@gmail.com>
2021-11-27 18:23:13 +0100

    27112021_18:23

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -47,6 +47,7 @@ function mostrarGastoWeb(idElemento, gasto) {
         divGastoEtiq.append(spanetiq);
     }
     );
+
     divG1.append(divGastoEtiq);
     if (idElemento == 'listado-gastos-completo') {
         let btnEditar = document.createElement('button');
@@ -203,43 +204,39 @@ function BorrarEtiquetasHandle() {
     }
 }
 
-function EditarHandleFormulario()
-{
-    this.handleEvent=function()
-    {
-        let g0=this.gasto;
-        let btnEditG=this.btnEditarGasto;
-        let divG0=this.divG1;
+function EditarHandleFormulario() {
+    this.handleEvent = function () {
+        let g0 = this.gasto;
+        let btnEditG = this.btnEditarGasto;
+        let divG0 = this.divG1;
 
-        this.btnEditarGasto.disabled=true;
+        this.btnEditarGasto.disabled = true;
         let plantillaFormulario = document.getElementById("formulario-template").content.cloneNode(true);
         let form = plantillaFormulario.querySelector("form");
 
-        form.elements.descripcion.value=g0.descripcion;
-        form.elements.valor.value=g0.valor;
-        form.elements.fecha.value=new Date(g0.fecha).toLocaleDateString();
-        form.elements.etiquetas.value=g0.etiquetas.toString();
+        form.elements.descripcion.value = g0.descripcion;
+        form.elements.valor.value = g0.valor;
+        form.elements.fecha.value = new Date(g0.fecha).toLocaleDateString();
+        form.elements.etiquetas.value = g0.etiquetas.toString();
         divG0.appendChild(form);
 
 
-        form.addEventListener("submit",this.handleEvent=function(event)
-        {
+        form.addEventListener("submit", this.handleEvent = function (event) {
             event.preventDefault();
             g0.actualizarDescripcion(form.elements.descripcion.value);
             g0.actualizarValor(parseFloat(form.elements.valor.value));
             g0.actualizarFecha(form.elements.fecha.value);
-            let etiquetas=form.elements.etiquetas;
-            etiquetas=etiquetas.value.split(",");
+            let etiquetas = form.elements.etiquetas;
+            etiquetas = etiquetas.value.split(",");
             g0.anyadirEtiquetas(...etiquetas);
-            btnEditG.disabled=false;
+            btnEditG.disabled = false;
             divG0.removeChild(form);
             repintar();
         });
 
-        let btnCancelar=form.querySelector("button.cancelar");
-        btnCancelar.addEventListener("click",this.handleEvent=function()
-        {
-            btnEditG.disabled=false;
+        let btnCancelar = form.querySelector("button.cancelar");
+        btnCancelar.addEventListener("click", this.handleEvent = function () {
+            btnEditG.disabled = false;
             divG0.removeChild(form);
             repintar();
         });
