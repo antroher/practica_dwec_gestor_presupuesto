2701be2108d56406caf28f4e56175ea2b74bb209
Joaquin Celis Abad <celis20987@iesmarenostrum.com>
2021-12-08 20:00:10 +0100

    21-12-08 mostrarGastoWeb

--- js/gestionPresupuestoweb.js
+++ js/gestionPresupuestoweb.js
@@ -9,4 +9,33 @@ function mostrarDatoEnId(idElemento,valor){
     item.innerHTML = `<p>${valor}</p>`  
 }
 
+function mostrarGastoWeb(idElemento, gasto){
+    
+    let elemento = document.getElementById(idElemento); // Captura del div con id dado
+    let spanEtiquetas = ""; 
+
+    // Generar el span de etiquetas iterando la prop etiquetas del objeto gasto
+    gasto.etiquetas.forEach((etiqueta) => {
+        spanEtiquetas +=
+            `<span class="gasto-etiquetas-etiqueta">
+            ${ etiqueta }
+            </span>`;
+    });
+
+    // Crear la estructura pedida, con divs para mostrar las prop y el str de spanEtiquetas
+    elemento.innerHTML +=
+        `<div class="gasto">
+                    <div class="gasto-descripcion">${gasto.descripcion}</div>
+                    <div class="gasto-fecha">${new Date(gasto.fecha).toLocaleString()}</div> 
+                    <div class="gasto-valor">${gasto.valor}</div> 
+                    <div class="gasto-etiquetas">
+                        ${spanEtiquetas}
+                    </div>
+        </div>`;
+
+}
+
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
+    
+}
 
--- js/pruebas_js.js
+++ js/pruebas_js.js
@@ -1,5 +1,51 @@
-let pb = "";
-pb = `<span class="clase">hola</span>`;
-let div = document.getElementById("presupuesto");
-div.innerHTML = pb;
-console.log(div);
+// let pb = "";
+// pb = `<span class="clase">hola</span>`;
+// let div = document.getElementById("presupuesto");
+// div.innerHTML = pb;
+// console.log(div);
+
+let divTarget = document.getElementById('presupuesto')
+
+const gasto = { descripcion: 'Monitor 20 Pulgadas', valor: 500, id: 5, fecha: '' };
+
+let spanEtiquetas = "";
+let divGasto = document.createElement('div');
+
+divGasto.classList.add('gasto');
+
+// divGasto.innerHTML = `<div class="gasto-descripcion">Tipo - ${gasto.nombre}</div>
+                    
+//                     <div class="gasto-valor">Valor - ${gasto.precio}</div>
+//                     <div class="gasto-id">id - ${gasto.id}</div>
+//                    `;
+// divTarget.append(divGasto);
+
+let elemento = document.getElementById('presupuesto')
+let etiquetas = 'etiquetas 0';
+elemento.innerHTML +=
+    `<div class="gasto">
+                    <div class="gasto-descripcion">${gasto.descripcion}</div>
+                    <div class="gasto-fecha">${new Date(gasto.fecha).toLocaleString()}</div> 
+                    <div class="gasto-valor">${gasto.valor}</div> 
+                    <div class="gasto-etiquetas">
+                        ${etiquetas}
+                    </div>
+        </div>`;
+
+
+
+
+
+
+
+
+// console.log(divTarget);
+
+/*
+`<div class="gasto">
+                    <div class="gasto-descripcion">${gasto.descripcion}</div>
+                    <div class="gasto-fecha">${new Date(gasto.fecha).toLocaleString()}</div> 
+                    <div class="gasto-valor">${gasto.valor}</div> 
+                    <div class="gasto-etiquetas">
+                        ${etiquetas}
+                    </div>*/
