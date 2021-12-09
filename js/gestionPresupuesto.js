10da77b25a2f5b612eb5908466ced555afe02a7c
Alberto Atienzar <albertoatienzargarcia@gmail.com>
2021-12-09 19:20:53 +0100

    comienzo actividad

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -235,6 +235,10 @@ function transformarListadoEtiquetas(etiquetas) {
     return etiquetafinal;
 }
 
+function cargarGastos (listarGastos){
+    gastos = listarGastos;
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -249,5 +253,6 @@ export   {
     calcularBalance,
     agruparGastos,
     filtrarGastos,
-    transformarListadoEtiquetas
+    transformarListadoEtiquetas,
+    cargarGastos
 }
