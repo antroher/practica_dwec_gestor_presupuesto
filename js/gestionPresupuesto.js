1282d619244676df37cca234af26ab634c91768c
Alina Maltceva <maltceva20537@iesmarenostrum.com>
2021-12-16 19:03:45 +0100

    Práctica 8, done!

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -196,6 +196,11 @@ function transformarListadoEtiquetas(txt)
     return txt.split(/[ ,;:\.~]+/g);
 }
 
+function cargarGastos(gastosCargar)
+{
+    gastosCargar.length >=0 && (gastos = gastosCargar);
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -210,5 +215,6 @@ export   {
     calcularBalance,
     filtrarGastos,
     agruparGastos,
-    transformarListadoEtiquetas
+    transformarListadoEtiquetas,
+    cargarGastos
 }
