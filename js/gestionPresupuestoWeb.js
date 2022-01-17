6ab4303ba018d172600830be7296e6f17b449a4a
Robert Hernández <hernandez19875@iesmarenostrum.com>
2022-01-17 16:42:08 +0100

    17012022_1647

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -161,7 +161,11 @@ function nuevoGastoWebFormulario(){
 
     final.append(formulario);
 
-    document.getElementById("anyadirgasto-formulario").disabled = true;
+    //document.getElementById("anyadirgasto-formulario").disabled = true;
+    //document.getElementById("button.gasto-editar-formulario").disabled = false;
+    document.getElementById("anyadirgasto-formulario").removeAttribute("disabled");
+
+    
 
     let enviar = new enviarGastoHandle();
 
@@ -188,7 +192,7 @@ function enviarGastoHandle() {
         let gasto1 = new gestionPresupuesto.CrearGasto(desc, val, fech, etiq);
 
         gestionPresupuesto.anyadirGasto(gasto1);
-        document.getElementById("anyadirgasto-formulario").disabled = true;
+        document.getElementById("anyadirgasto-formulario").disabled = false;
         repintar();
     }
 }
