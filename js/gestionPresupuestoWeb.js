22e74215053006823ec32454ebe42245d43bc913
Ricardo Hernandez <hernandez16437@iesmarenostrum.com>
2022-01-25 16:10:14 +0100

    25012022 empezando practica 10

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -95,6 +95,12 @@ function mostrarGastoWeb(idElemento, gasto) {
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+    // Obtener la capa donde se muestran los datos agrupados por el período indicado.
+    // Seguramente este código lo tengas ya hecho pero el nombre de la variable sea otro.
+    // Puedes reutilizarlo, por supuesto. Si lo haces, recuerda cambiar también el nombre de la variable en el siguiente bloque de código
+    var divP = document.getElementById(idElemento);
+    // Borrar el contenido de la capa para que no se duplique el contenido al repintar
+    divP.innerHTML = "";
     const elemento = document.getElementById(idElemento);
     let data = ""
     for (let [key, value] of Object.entries(agrup)) {
