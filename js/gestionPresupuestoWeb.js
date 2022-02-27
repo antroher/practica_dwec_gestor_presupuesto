aa7343097952b92f71f05cc0343e0ed16fa993f0
ismaelproyects <ismaelamo21@gmail.com>
2022-02-27 13:18:47 +0100

    Ahora si 10Finalizada

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -169,6 +169,7 @@ function mostrarDatoEnId(idElemento,valor){
         function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo){
             let elemento = document.getElementById(idElemento);
 
+            elemento.innerHTML= "";
             //bucle tocho
           let gastos ="";
             for(let prop in agrup){
@@ -183,7 +184,69 @@ function mostrarDatoEnId(idElemento,valor){
             `<div class='agrupacion'> 
             <h1>Gastos agrupados por ${periodo} </h1>
             ${gastos}`;
+
+            //PRACTICA 10
+              // Estilos
+                    elemento.style.width = "33%";
+                    elemento.style.display = "inline-block";
+                    // Crear elemento <canvas> necesario para crear la gráfica
+                    // https://www.chartjs.org/docs/latest/getting-started/
+                    let chart = document.createElement("canvas");
+                    // Variable para indicar a la gráfica el período temporal del eje X
+                    // En función de la variable "periodo" se creará la variable "unit" (anyo -> year; mes -> month; dia -> day)
+                    let unit = "";
+                    switch (periodo) {
+                    case "anyo":
+                        unit = "year";
+                        break;
+                    case "mes":
+                        unit = "month";
+                        break;
+                    case "dia":
+                    default:
+                        unit = "day";
+                        break;
+                }
+
+         // Creación de la gráfica
+    // La función "Chart" está disponible porque hemos incluido las etiquetas <script> correspondientes en el fichero HTML
+    const myChart = new Chart(chart.getContext("2d"), {
+        // Tipo de gráfica: barras. Puedes cambiar el tipo si quieres hacer pruebas: https://www.chartjs.org/docs/latest/charts/line.html
+        type: 'bar',
+        data: {
+            datasets: [
+                {
+                    // Título de la gráfica
+                    label: `Gastos por ${periodo}`,
+                    // Color de fondo
+                    backgroundColor: "#555555",
+                    // Datos de la gráfica
+                    // "agrup" contiene los datos a representar. Es uno de los parámetros de la función "mostrarGastosAgrupadosWeb".
+                    data: agrup
+                }
+            ],
+        },
+        options: {
+            scales: {
+                x: {
+                    // El eje X es de tipo temporal
+                    type: 'time',
+                    time: {
+                        // Indicamos la unidad correspondiente en función de si utilizamos días, meses o años
+                        unit: unit
+                    }
+                },
+                y: {
+                    // Para que el eje Y empieza en 0
+                    beginAtZero: true
+                }
+            }
         }
+    });
+    // Añadimos la gráfica a la capa
+    elemento.append(chart);
+
+    }
 
 
         //Funcion repintar para actualizar la pagina
@@ -195,6 +258,15 @@ function mostrarDatoEnId(idElemento,valor){
                 document.getElementById("listado-gastos-completo").innerHTML = " ";      //Bora el contenido sustituyendolo por un string ("")
 
                 mostrarGastoWeb("listado-gastos-completo",GesPresu.listarGastos());
+
+                let Dia = GesPresu.agruparGastos("dia");
+    mostrarGastosAgrupadosWeb("agrupacion-dia", Dia, "día");
+
+    let Mes = GesPresu.agruparGastos("mes");
+    mostrarGastosAgrupadosWeb("agrupacion-mes", Mes, "mes");
+
+    let Anyo = GesPresu.agruparGastos("anyo");
+    mostrarGastosAgrupadosWeb("agrupacion-anyo", Anyo, "año");
             }
 
             
