91a92d6effdcd7967ff5e8fdcb7085839fd28184
Joaquin Celis Abad <celis20987@iesmarenostrum.com>
2022-02-27 09:33:49 +0100

    2022-02-02 problema push

--- js/pruebas_js.js
+++ js/pruebas_js.js
@@ -3,56 +3,33 @@
 import * as gestionP from "./gestionPresupuesto.js";
 
 
-// const gasto1 = new gestionP.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
-// const gasto2 = new gestionP.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
 
 
+// localStorage.user = JSON.stringify({ name: "John" });
+// console.log(localStorage.user);
+// let user = JSON.parse(localStorage.user);
+// console.log(user.name);
 
-// let gasto = {id: 12};
-// console.log(gasto);
-
-// let btnEditForm = document.createElement("button");
-// btnEditForm.className = 'gasto-editar-formulario';
-// btnEditForm.id = `gasto-editar-formulario-${gasto.id}`;
-// btnEditForm.textContent = 'Editar (Formulario)';
-
-// aplicacion.append(btnEditForm);
+// alert(`${key}: ${localStorage.getItem(key)}`);
 
 
-/*
-let formulario = document.querySelector('#controlesprincipales');
-formulario.querySelector("button.cancelar").addEventListener('click', cancelarHandler);
+var formulario = document.querySelector('#formpeliculas');
+formulario.addEventListener('submit', function(){
 
-function cancelarHandler(e){
-    alert("hola");
-}
-*/
+    var titulo = document.querySelector('#addpelicula').value;
 
+    if (titulo.length >= 1){
+        localStorage.setItem(titulo, titulo);
+    }         
+});
 
-
-/*
-var presupuesto = 0;
-var gastos = [];
-var idGasto = 0;
-const gasto1 = new gestionP.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
-const gasto2 = new gestionP.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
-
-function anyadirGasto(gasto) {
-    if (typeof (gasto) === "object") {
-        gasto.id = idGasto;
-        gastos.push(gasto);
-        idGasto++;
+let ul = document.querySelector('ul');
+for (let i in localStorage) {
+    if (typeof localStorage[i] == 'string') {
+        console.log(localStorage[i]);
+        let li = document.createElement('li');
+        li.append(localStorage[i]);
+        ul.append(li);
     }
-}
 
-anyadirGasto(gasto1);
-anyadirGasto(gasto2);
-console.log(gasto1);
-console.log(gasto2);
-*/
-
-localStorage.user = JSON.stringify({ name: "John" });
-console.log(localStorage.user);
-let user = JSON.parse(localStorage.user);
-console.log(user.name);
-
+} 
