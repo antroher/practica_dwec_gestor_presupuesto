bf9beb62ecc8bbba0e5f27e11c3cbe1f308dfbb6
Senen Rodenas <71589700+Senen1997@users.noreply.github.com>
2022-03-04 10:44:42 +0100

    Se me ha olvidado borrar una prueba y por eso daba fallo en el test 1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -17,7 +17,7 @@ function actualizarPresupuesto(newPresupuesto) {
         console.log("Error. Valor introducido no valido.");
         newValor = -1;
     }
-    return newValor.toFixed;
+    return newValor;
 }
 
 function mostrarPresupuesto() {
